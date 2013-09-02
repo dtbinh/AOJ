64bd0aeb961d3a353bfa7c85eba642b7da252fc4
okaoka <s1220795@u.tsukuba.ac.jp>
2013-09-03 04:51:14 +0900

    wrote check function, but it would be more slowly

--- Volume_12/1295_Cubist_Artwork.cpp
+++ Volume_12/1295_Cubist_Artwork.cpp
@@ -31,13 +31,36 @@ const static int ty[] = {-1,0,1,0};
 static const double EPS = 1e-8;
 
 int stage[20][20];
+bool visited[20][20][20];
 int gRes = INF;
 
+bool check(int col_num,int row_num,int col[32],int row[32]){
+  for(int x=0;x<col_num;x++){
+    int max_h = 0;
+    for(int y=0;y<row_num;y++){
+      max_h = max(stage[y][x],max_h);
+    }
+    if(max_h != col[x]) return false;
+  }
+  for(int y=0;y<row_num;y++){
+    int max_h = 0;
+    for(int x=0;x<col_num;x++){
+      max_h = max(stage[y][x],max_h);
+    }
+    if(max_h != row[y]) return false;
+  }
+  return true;
+}
+
 void dfs(int col_num,int row_num,
-	 int max_h,int pos,
+	 int pos,
 	 int col[32],int row[32],int total){
   if(pos == col_num * row_num){
-    gRes = min(gRes,total);
+    // printf("%d\n",total);
+    if(check(col_num,row_num,col,row)){
+      gRes = min(gRes,total);
+      printf("%d\n",gRes);
+    }
     return;
   }
 
@@ -45,13 +68,17 @@ void dfs(int col_num,int row_num,
   int y = pos / col_num;
 
   stage[y][x] = 0;
-  dfs(col_num,row_num,max_h,pos+1,col,row,total+0);
-
-  stage[y][x] = col[x];
-  dfs(col_num,row_num,max_h,pos+1,col,row,total+col[x]);
-
-  stage[y][x] = row[y];
-  dfs(col_num,row_num,max_h,pos+1,col,row,total+row[y]);
+  dfs(col_num,row_num,pos+1,col,row,total);
+  
+  if(total + col[x] < gRes){
+    stage[y][x] = col[x];
+    dfs(col_num,row_num,pos+1,col,row,total+col[x]);
+  }
+  
+  if(total + row[y] < gRes){
+    stage[y][x] = row[y];
+    dfs(col_num,row_num,pos+1,col,row,total+row[y]);
+  }
 }
 
 int main(){
@@ -60,21 +87,19 @@ int main(){
   
   while(~scanf("%d %d",&col_num,&row_num)){
     memset(stage,0,sizeof(stage));
+    memset(visited,false,sizeof(visited));
     gRes = INF;
     memset(col,0,sizeof(col));
     memset(row,0,sizeof(row));
 
-    int max_h = 0;
     for(int i=0;i<col_num;i++){
       scanf("%d",col+i);
-      max_h = max(col[i],max_h);
     }
     for(int i=0;i<row_num;i++){
       scanf("%d",row+i);
-      max_h = max(row[i],max_h);
     }
     
-    dfs(col_num,row_num,max_h,0,col,row,0);
+    dfs(col_num,row_num,0,col,row,0);
     printf("%d\n",gRes);
   }
 }
