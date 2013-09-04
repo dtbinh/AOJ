00db99a2ade0abdd9611bc5f05e95ae0202119c3
okaoka <s1220795@u.tsukuba.ac.jp>
2013-09-05 01:22:20 +0900

    solve 1295_Cubist_Artwork

--- Volume_12/1295_Cubist_Artwork.cpp
+++ Volume_12/1295_Cubist_Artwork.cpp
@@ -30,77 +30,38 @@ const static int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
-int stage[20][20];
-bool visited[20][20][20];
-int gRes = INF;
-
-bool check(int col_num,int row_num,int col[32],int row[32]){
-  for(int x=0;x<col_num;x++){
-    int max_h = 0;
-    for(int y=0;y<row_num;y++){
-      max_h = max(stage[y][x],max_h);
-    }
-    if(max_h != col[x]) return false;
-  }
-  for(int y=0;y<row_num;y++){
-    int max_h = 0;
-    for(int x=0;x<col_num;x++){
-      max_h = max(stage[y][x],max_h);
-    }
-    if(max_h != row[y]) return false;
-  }
-  return true;
-}
-
-void dfs(int col_num,int row_num,
-	 int pos,
-	 int col[32],int row[32],int total){
-  if(pos == col_num * row_num){
-    // printf("%d\n",total);
-    if(check(col_num,row_num,col,row)){
-      gRes = min(gRes,total);
-      printf("%d\n",gRes);
-    }
-    return;
-  }
-
-  int x = pos % col_num;
-  int y = pos / col_num;
-
-  stage[y][x] = 0;
-  dfs(col_num,row_num,pos+1,col,row,total);
-  
-  if(total + col[x] < gRes){
-    stage[y][x] = col[x];
-    dfs(col_num,row_num,pos+1,col,row,total+col[x]);
-  }
-  
-  if(total + row[y] < gRes){
-    stage[y][x] = row[y];
-    dfs(col_num,row_num,pos+1,col,row,total+row[y]);
-  }
-}
-
 int main(){
   int col_num,row_num;
   int col[32],row[32];
+  int freq[32];
   
   while(~scanf("%d %d",&col_num,&row_num)){
-    memset(stage,0,sizeof(stage));
-    memset(visited,false,sizeof(visited));
-    gRes = INF;
+    if(col_num == 0 && row_num == 0) break;
+
     memset(col,0,sizeof(col));
     memset(row,0,sizeof(row));
+    memset(freq,0,sizeof(freq));
 
+    int total = 0;
     for(int i=0;i<col_num;i++){
+      int num;
       scanf("%d",col+i);
+      freq[col[i]]++;
+      total += col[i];
     }
+
     for(int i=0;i<row_num;i++){
       scanf("%d",row+i);
+      if(freq[row[i]]>0) {
+	freq[row[i]]--;
+	continue;
+      }
+      else{
+	total += row[i];
+      }
     }
     
-    dfs(col_num,row_num,0,col,row,0);
-    printf("%d\n",gRes);
+    printf("%d\n",total);
   }
 }
 
