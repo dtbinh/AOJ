6a07fab17c75b1ebc2632166c95169f814ed1727
okaoka <s1220795@u.tsukuba.ac.jp>
2013-09-27 11:07:56 +0900

    solve 2021_Princess_in_Danger

--- Volume_20/2021_Princess_in_Danger.cpp
+++ Volume_20/2021_Princess_in_Danger.cpp
@@ -1,7 +1,7 @@
 #define _USE_MATH_DEFINES
 #define INF 0x3f3f3f3f
-#include <cstdio>
 #include <iostream>
+#include <cstdio>
 #include <sstream>
 #include <cmath>
 #include <cstdlib>
@@ -36,32 +36,19 @@ int dp[100][101]; //dp[pos][life] := min_cost
 int city_num,refreezing_limit,freezing_plant_num,
   road_num,capital_idx,hospital_idx;
 
-void dfs(int from,int life,int cost_sum){
-
-  for(int to=0;to<city_num;to++){
-    if(from == to) continue;
-
-    if(is_freezing_plant[from]){
-      for(int stay=0;stay+life<=refreezing_limit;stay++){
-	if(stay+life-cost[from][to] < 0) continue;
-
-	int remaining_life = min(life-cost[from][to]+stay,refreezing_limit);
-	if(dp[to][remaining_life] > cost_sum+cost[from][to]+stay){
-	  dp[to][remaining_life] = cost_sum+cost[from][to]+stay;
-	  dfs(to,remaining_life,cost_sum+cost[from][to]+stay);
-	}
-      }
-    }
-    else{
-      if(life-cost[from][to] < 0) continue;
-      int remaining_life = life-cost[from][to];
-      	if(dp[to][remaining_life] > cost_sum+cost[from][to]){
-	  dp[to][remaining_life] = cost_sum + cost[from][to];
-	  dfs(to,remaining_life,cost_sum+cost[from][to]);
-	}
-    }
+class Node{
+public:
+  Node(int _l,int _p,int _c) : life(_l),pos(_p),cost_sum(_c){}
+  int life;
+  int pos;
+  int cost_sum;
+  bool operator<(const Node& n) const{
+    return cost_sum < n.cost_sum;
   }
-}
+  bool operator>(const Node& n) const{
+    return cost_sum > n.cost_sum;
+  }
+};
 
 int main(){
   while(~scanf("%d %d %d %d %d %d",
@@ -89,8 +76,44 @@ int main(){
 
     memset(dp,0x3f,sizeof(dp));
     dp[capital_idx][refreezing_limit] = 0;
-    dfs(capital_idx,refreezing_limit,0);
 
+    Node n(refreezing_limit,capital_idx,0);
+    priority_queue<Node,vector<Node>,greater<Node> > que;
+    que.push(n);
+    
+    while(!que.empty()){
+      Node n = que.top();
+      que.pop();
+      int from = n.pos;
+      int life = n.life;
+      int cost_sum = n.cost_sum;
+
+      for(int to=0;to<city_num;to++){
+	if(from == to) continue;
+
+	if(is_freezing_plant[from]){
+	  for(int stay=refreezing_limit-life;stay>=0;stay--){
+	    if(stay+life-cost[from][to] < 0) break;
+
+	    int remaining_life = life-cost[from][to]+stay;
+	    if(dp[to][remaining_life] > cost_sum+cost[from][to]+stay){
+	      dp[to][remaining_life] = cost_sum+cost[from][to]+stay;
+	      que.push(Node(remaining_life,to,cost_sum+cost[from][to]+stay));
+	    }
+	  }
+	}
+	else{
+	  if(life <= 0) break;
+	  if(life-cost[from][to] < 0) continue;
+	  int remaining_life = life-cost[from][to];
+	  if(dp[to][remaining_life] > cost_sum+cost[from][to]){
+	    dp[to][remaining_life] = cost_sum + cost[from][to];
+	    que.push(Node(remaining_life,to,cost_sum+cost[from][to]));
+	  }
+	}
+      }
+    }
+    
     int res = INF;
     for(int i=0;i<=refreezing_limit;i++){
       res = min(res,dp[hospital_idx][i]);
