a1b4d90e06fb9b331b680eeb89d7f25929c08aeb
okaoka <s1220795@u.tsukuba.ac.jp>
2013-10-05 20:51:16 +0900

    little bit wrong

--- Volume_21/2166_Erratic_Sleep_Habits.cpp
+++ Volume_21/2166_Erratic_Sleep_Habits.cpp
@@ -36,35 +36,48 @@ int main(){
   int cycle[100];
   map<int,vector<int> > interview;
 
-  scanf("%d",&cycle_length);
-  for(int i=0;i<cycle_length;i++){
-    scanf("%d",cycle+i);
-  }
-  scanf("%d",&interview_num);
+  while(~scanf("%d",&cycle_length)){
+    if(cycle_length == 0) break;
+    for(int i=0;i<cycle_length;i++){
+      scanf("%d",cycle+i);
+    }
+    scanf("%d",&interview_num);
   
-  int last_day = 0;
-  for(int i=0;i<interview_num;i++){
-    int day,start_hour;
-    scanf("%d %d",&day,&start_hour);
-    interview[day].push_back(start_hour);
-    last_day = max(day,last_day);
-  }
+    int last_day = 0;
+    for(int i=0;i<interview_num;i++){
+      int day,start_hour;
+      scanf("%d %d",&day,&start_hour);
+      interview[day].push_back(start_hour);
+      last_day = max(day,last_day);
+    }
 
-  int dp[100][100];
-  memset(dp,0,sizeof(dp));
+    //dp[day][cycle_idx] = min_cost
+    int dp[101][100];
+    memset(dp,0x3f,sizeof(dp));
 
-  //1
-  //0
-  //1 1
-  //0 1
-  //1 0
-  //0 0
+    //init
+    dp[1][0] = 0;
+    for(int i=0;i<cycle_length;i++){
+      dp[0][i] = 0;
+    }
 
-  for(int day=1;day<=last_day;day++){
-    for(int used_day=1;used_day<day;used_day++){
-      for(int hour_idx=0;hour_idx<interview[day].size();hour_idx++){
-	int start_hour = interview[hour_idx];
+    for(int day=1;day<=last_day;day++){
+      for(int cycle_idx=0;cycle_idx < cycle_length; cycle_idx++){
+	for(int hour_idx=0;hour_idx<interview[day].size();hour_idx++){
+	  int start_hour = interview[day][hour_idx];
+	  if(cycle[(cycle_idx+1)%cycle_length] <= start_hour){
+	    dp[day][(cycle_idx+1)%cycle_length]
+	      = min(dp[day-1][cycle_idx],dp[day][(cycle_idx+1)%cycle_length]);
+	  }
+	  dp[day][0] = min(dp[day-1][cycle_idx]+1,dp[day][0]);
+	}
       }
     }
+  
+    int min_v = INF;
+    for(int i=0;i<cycle_length;i++){
+      min_v = min(dp[last_day][i],min_v);
+    }
+    printf("%d\n",min_v);
   }
 }
