1e121d45e5ffbfa7396616faf33c4682014ffe8e
okaoka <s1220795@u.tsukuba.ac.jp>
2013-10-07 12:06:27 +0900

    wrote reading section

---
+++ Volume_12/1251_Pathological_Paths.cpp
@@ -0,0 +1,46 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {0,1,0,-1};
+int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int total_reference;
+  int total_suspicious;
+  while(~scanf("%d %d",&total_reference,&total_suspicious)){
+    for(int i=0;i<total_reference;i++){
+      string url;
+      cin >> url;
+    }
+    for(int i=0;i<total_suspicious;i++){
+      string url;
+      cin >> url;
+    }
+  }
+}
