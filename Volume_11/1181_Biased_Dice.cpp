8ce177d2231adda5f9f8ac6042a036454babf1ca
okaoka <s1220795@u.tsukuba.ac.jp>
2013-10-16 15:41:12 +0900

    wrote roughly

--- Volume_11/1181_Biased_Dice.cpp
+++ Volume_11/1181_Biased_Dice.cpp
@@ -30,17 +30,18 @@ int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
+enum dir {
+  TOP,
+  FRONT,
+  REAR,
+  BOTTOM,
+  LEFT,
+  RIGHT
+};
+
 class Dice {
 public:
   int surface[6];
-  enum dir {
-    TOP,
-    FRONT,
-    REAR,
-    BOTTOM,
-    LEFT,
-    RIGHT
-  };
 
   Dice(int top,int front){
     surface[TOP] = 1;
@@ -61,7 +62,7 @@ public:
 	  roll(k);
 	  if(surface[TOP] == top
 	     && surface[FRONT] == front){
-	    goto found;
+	    goto complete;
 	  }
 
 	  memcpy(surface,tmp,sizeof(int)*6);
@@ -69,7 +70,7 @@ public:
       }
     }
 
-  found:;
+  complete:;
   }
 
   
@@ -99,6 +100,7 @@ public:
   }
   void roll(int num){
     //TOP,LEFT,BOTTOM,RIGHT
+    for(int i=0;i<num;i++){
       int next[6];
       memcpy(next,surface,sizeof(int)*6);
       next[TOP] = surface[LEFT];
@@ -106,6 +108,7 @@ public:
       next[BOTTOM] = surface[RIGHT];
       next[RIGHT] = surface[TOP];
       memcpy(surface,next,sizeof(int)*6);
+    }
   }
 };
 
@@ -115,13 +118,72 @@ int main(){
   while(~scanf("%d",&n)){
     if(n==0) break;
 
-    int stage[101][3];
-    
+    deque<int> stage[5];
+
     for(int i=0;i<n;i++){
       int top,front;
       scanf("%d %d",&top,&front);
       Dice dice(top,front);
+      
+      if(i==0){
+	stage[0].push_back(dice.surface[TOP]);
+	continue;
+      }
 
+      //check stage
+      //1 2
+      // 0
+      //3 4
+      bool isok = false;
+      for(int num=6;num>=4;num--){
+	if(dice.surface[FRONT] == num){
+	  if(stage[3].size() < stage[0].size()){
+	    stage[3].push_back(dice.surface[REAR]);
+	    isok = true;
+	    goto found;
+	  }
+	}
+	else if(dice.surface[REAR] == num){
+	  if(stage[2].size() < stage[0].size()){
+	    stage[2].push_back(dice.surface[TOP]);
+	    isok = true;
+	    goto found;
+	  }
+	}
+	else if(dice.surface[LEFT] == num){
+	  if(stage[4].size() < stage[0].size()){
+	    stage[4].push_back(dice.surface[RIGHT]);
+	    isok = true;
+	    goto found;
+	      
+	  }
+	}
+	else if(dice.surface[RIGHT] == num){
+	  if(stage[1].size() < stage[0].size()){
+	    stage[1].push_back(dice.surface[LEFT]);
+	    isok = true;
+	    goto found;
+	  }
+	}
+      }
+    found:;
+      if(!isok){
+	stage[0].push_back(dice.surface[TOP]);
+      }
     }
+
+    int freq[7];
+    memset(freq,0,sizeof(freq));
+    
+    for(int i=0;i<5;i++){
+      if(stage[i].size() > 0){
+	freq[stage[i][stage[i].size()-1]]++;
+      }
+    }
+
+    for(int i=1;i<=6;i++){
+      printf("%s%d",i==1 ? "" : " ", freq[i]);
+    }
+    printf("\n");
   }
 }
