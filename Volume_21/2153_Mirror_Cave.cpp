1026c20be6d029da10e4a30d8372e6dccf65c081
okaoka <s1220795@u.tsukuba.ac.jp>
2013-10-20 17:04:52 +0900

    fix memorization method

--- Volume_21/2153_Mirror_Cave.cpp
+++ Volume_21/2153_Mirror_Cave.cpp
@@ -34,55 +34,14 @@ static const int ty_r[] = {-1,0,1,0};
 static const int tx_l[] = {0,-1,0,1};
 static const int ty_l[] = {-1,0,1,0};
 
-bool Rin_visited[50][50];
-bool Len_visited[50][50];
+bool visited[50][50][50][50];
 char stage_Len[50][50];
 char stage_Rin[50][50];
 
 bool can_reach;
 int W,H;
 
-void Rin_dfs(int Rin_x,int Rin_y,int Len_x,int Len_y){
-  for(int i=0;i<4;i++){
-    int Rin_dx = Rin_x + tx_r[i];
-    int Rin_dy = Rin_y + ty_r[i];
-
-    int Len_dx = Len_x + tx_l[i];
-    int Len_dy = Len_y + ty_l[i];
-    if(Rin_dx < 0 || Rin_dx >= W || Rin_dy < 0 || Rin_dy >= H){
-	continue;
-    }
-    if(Len_dx < 0 || Len_dx >= W || Len_dy < 0 || Len_dy >= H){
-	continue;
-    }
-    if(stage_Rin[Rin_dy][Rin_dx] == '#'){
-      continue;
-    }
-    if(stage_Len[Len_dy][Len_dx] == '#'){
-      Len_dy = Len_y;
-      Len_dx = Len_x;
-    }
-
-    if(Rin_visited[Rin_dy][Rin_dx]){
-      continue;
-    }
-
-
-    if(stage_Len[Len_dy][Len_dx] == '%'
-       && stage_Rin[Rin_dy][Rin_dx] == '%'){
-      can_reach = true;
-      return;
-    }
-
-    if(stage_Len[Len_dy][Len_dx] != '%'
-       && stage_Rin[Rin_dy][Rin_dx] != '%'){
-      Rin_visited[Rin_dy][Rin_dx] = true;
-      Rin_dfs(Rin_dx,Rin_dy,Len_dx,Len_dy);
-    }
-  }
-}
-
-void Len_dfs(int Rin_x,int Rin_y,int Len_x,int Len_y){
+void dfs(int Rin_x,int Rin_y,int Len_x,int Len_y){
   for(int i=0;i<4;i++){
     int Rin_dx = Rin_x + tx_r[i];
     int Rin_dy = Rin_y + ty_r[i];
@@ -100,14 +59,14 @@ void Len_dfs(int Rin_x,int Rin_y,int Len_x,int Len_y){
       Rin_dx = Rin_x;
     }
     if(stage_Len[Len_dy][Len_dx] == '#'){
+      Len_dy = Len_y;
+      Len_dx = Len_x;
+    }
+
+    if(visited[Rin_dx][Rin_dy][Len_dx][Len_dy]){
       continue;
     }
 
-    if(Len_visited[Len_dy][Len_dx]){
-      continue;
-    }
-
-    Len_visited[Len_dy][Len_dx] = true;
     if(stage_Len[Len_dy][Len_dx] == '%'
        && stage_Rin[Rin_dy][Rin_dx] == '%'){
       can_reach = true;
@@ -116,8 +75,8 @@ void Len_dfs(int Rin_x,int Rin_y,int Len_x,int Len_y){
 
     if(stage_Len[Len_dy][Len_dx] != '%'
        && stage_Rin[Rin_dy][Rin_dx] != '%'){
-      Len_visited[Len_dy][Len_dx] = true;
-      Len_dfs(Rin_dx,Rin_dy,Len_dx,Len_dy);
+      visited[Rin_dx][Rin_dy][Len_dx][Len_dy] = true;
+      dfs(Rin_dx,Rin_dy,Len_dx,Len_dy);
     }
   }
 }
@@ -127,8 +86,16 @@ int main(){
   while(~scanf("%d %d",&W,&H)){
     if(W==0 && H==0) break;
 
-    memset(Len_visited,false,sizeof(Len_visited));
-    memset(Rin_visited,false,sizeof(Rin_visited));
+    for(int y=0;y<H;y++){
+      for(int x=0;x<W;x++){
+	for(int y2=0;y2<H;y2++){
+	  for(int x2=0;x2<W;x2++){
+	    visited[x][y][x2][y2] = false;
+	  }
+	}
+      }
+    }
+
     can_reach = false;
     
     int Rin_start_x = 0;
@@ -158,8 +125,7 @@ int main(){
       }
     }
 
-    Len_dfs(Rin_start_x,Rin_start_y,Len_start_x,Len_start_y);
-    Rin_dfs(Rin_start_x,Rin_start_y,Len_start_x,Len_start_y);
+    dfs(Rin_start_x,Rin_start_y,Len_start_x,Len_start_y);
     printf("%s\n",can_reach ? "Yes" : "No");
   }
 }
