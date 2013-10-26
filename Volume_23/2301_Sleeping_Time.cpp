97d4457c2d6608fd35f897c8ff99b73f3e59d219
okaoka <s1220795@u.tsukuba.ac.jp>
2013-10-26 13:10:11 +0900

    MLE for the huge size of the vector

--- Volume_23/2301_Sleeping_Time.cpp
+++ Volume_23/2301_Sleeping_Time.cpp
@@ -30,51 +30,63 @@ const static int ty[] = {-1,-1,0,1,1,1,0,-1};
  
 static const double EPS = 1e-10;
 
+struct RL{
+  double R;
+  double L;
+  double probability;
+  RL(double _r,double _l,double _p) : R(_r),L(_l),probability(_p) {}
+};
+
 int main(){
   int total_nights;
   int R,L;
-  int dp[31][13][13]; //dp[day][R][L] := probability
-  
+
   while(~scanf("%d %d %d",&total_nights,&R,&L)){
-    memset(dp,0,sizeof(dp));
-    dp[0][R][L] = 1.0;
+    vector<RL> dp[31]; //dp[day][idx].{R,L} := probability
+    dp[0].push_back(RL(R,L,1.0));
 
     double mistake_probability,reference_diff,optimal_sleep_time;
     scanf("%lf %lf %lf",&mistake_probability,&reference_diff,&optimal_sleep_time);
+    
     for(int day=1;day<=total_nights;day++){
-      for(int R=0;R<=12;R++){
-	for(int L=0;L<=12;L++){
-	  int H = (R+L)/2;
-	  if(H >= optimal_sleep_time){
-	    int correct_next_L = H;
-	    dp[day][R][correct_next_L] += dp[day-1][R][L] * (1.0-mistake_probability);
+      for(int i=0;i<dp[day-1].size();i++){
+	double H = (dp[day-1][i].R+dp[day-1][i].L)/2.0;
 
-	    int wrong_next_R = H;
-	    dp[day][wrong_next_R][L] += dp[day-1][R][L] * mistake_probability;
-	  }
-	  else{
-	    int correct_next_R = H;
-	    dp[day][correct_next_R][L] += dp[day-1][R][L] * (1.0-mistake_probability);
+	if(H >= optimal_sleep_time){
+	  double correct_next_L = H;
+	  RL rl_c(dp[day-1][i].R,correct_next_L,
+		  dp[day-1][i].probability * (1.0-mistake_probability));
+	  dp[day].push_back(rl_c);
 
-	    int wrong_next_L = H;
-	    dp[day][R][wrong_next_L] += dp[day-1][R][L] * mistake_probability;
-	  }
+	  double wrong_next_R = H;
+	  RL rl_w(wrong_next_R,dp[day-1][i].L,
+		  dp[day-1][i].probability * mistake_probability);
+	  dp[day].push_back(rl_w);
+	}
+	else{
+	  double correct_next_R = H;
+	  RL rl_c(correct_next_R,dp[day-1][i].L,
+		  dp[day-1][i].probability * (1.0-mistake_probability));
+	  dp[day].push_back(rl_c);
+
+	  double wrong_next_L = H;
+	  RL rl_w(dp[day-1][i].R,wrong_next_L,
+		  dp[day-1][i].probability * mistake_probability);
+	  dp[day].push_back(rl_w);
 	}
       }
+
+      dp[day-1].clear();
     }
 
-    
     int last_night_day = total_nights;
-
     double prob = 0.0;
-    for(int R=0;R<=12;R++){
-      for(int L=0;L<=12;L++){
-	if(abs(optimal_sleep_time - (R+L)/2) <= reference_diff){
-	  prob += dp[last_night_day][R][L];
-	}
+    for(int i=0;i<dp[last_night_day].size();i++){
+      // cout << dp[last_night_day][i].R << " " << dp[last_night_day][i].L << " " <<dp[last_night_day][i].probability << endl;
+      if(abs(optimal_sleep_time - (dp[last_night_day][i].R+dp[last_night_day][i].L)/2.0) <= reference_diff){
+	prob += dp[last_night_day][i].probability;
       }
     }
-    
     printf("%lf\n",prob);
   }
 }
