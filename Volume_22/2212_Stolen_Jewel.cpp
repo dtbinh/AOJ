6231a460704afc83722f45d646e83fc103c27dda
okaoka <s1220795@u.tsukuba.ac.jp>
2013-12-15 10:29:02 +0900

    set<int> is useless

--- Volume_22/2212_Stolen_Jewel.cpp
+++ Volume_22/2212_Stolen_Jewel.cpp
@@ -32,182 +32,66 @@ int ty[] = {-1,0,1,0};
 static const double EPS = 1e-8;
 
 //URDL
-const static char dir[4] = {'U','R','D','L'};
-const static string dir_str[4] = {"U","R","D","L"};
+// const static char dir[4] = {'U','R','D','L'};
+// const static string dir_str[4] = {"U","R","D","L"};
 
-namespace AhoCorasick{
-  class Node;
-  class SearchMachine;
+class Nodes {
+public:
+  class Nodes* children[256];
+  bool has_word;
+  Nodes() : has_word(false){
+    for(int i=0;i<256;i++){
+      children[i] = NULL;
+    }
+  }
 };
 
-class AhoCorasick::Node {
+class Trie {
 private:
-  set<string> results;
-  map<char,AhoCorasick::Node*> transitions;
-  vector<AhoCorasick::Node*> v_transitions;
-  char character;
-  AhoCorasick::Node* parent;
-  AhoCorasick::Node* failure;
+  Nodes* root;
 public:
-  Node() : character('\0'),parent(NULL),failure(NULL){}
-  Node(AhoCorasick::Node* _p,char _c) : parent(_p),character(_c),failure(NULL){}
-  const char get_char() const {
-    return character;
-  }
-  
-  AhoCorasick::Node* get_parent() const{
-    return parent;
+  Trie(){
+    root = new Nodes();
   }
 
-  AhoCorasick::Node* get_failure() const{
-    return failure;
-  }
-
-  void set_failure(AhoCorasick::Node* _n){
-    failure = _n;
-  }
-
-  AhoCorasick::Node* get_transition(char c){
-    if(transitions.find(c) == transitions.end()) return NULL;
-    return transitions[c];
-  }
-
-  const set<string>& get_results() const{
-    return results;
-  }
-
-  void add_result(const string& str){
-    results.insert(str);
-  }
-
-  void add_transition(AhoCorasick::Node* node){
-    transitions[node->get_char()] = node;
-    v_transitions.push_back(node);
-  }
-
-  const vector<AhoCorasick::Node*>& get_transitions() const{
-    return v_transitions;
-  }
-  
-};
-class AhoCorasick::SearchMachine{
-private:
-  set<string> keywords;
-  AhoCorasick::Node* root;
-  AhoCorasick::Node* state;
-public:
-  SearchMachine(set<string> _k) : keywords(_k){
-    _build_tree();
-  }
-  SearchMachine(){
-    _build_tree();
-  }
-  void _build_tree(){
-    root = new AhoCorasick::Node();
-    
-    for(set<string>::iterator it = keywords.begin();
-	it != keywords.end();
-	it++){
-      AhoCorasick::Node* node = root;
-      const string& keyword = *it;
-      for(int i = 0; i < keyword.length(); i++){
-	AhoCorasick::Node* next_node = node->get_transition(keyword[i]);
-	if(next_node == NULL){
-	  next_node = new AhoCorasick::Node(node,keyword[i]);
-	  node->add_transition(next_node);
-	}
-	node = next_node;
+  void insert(const string& str){
+    Nodes* current = root;
+    for(int i=0;i<str.size();i++){
+      char c = str[i];
+      if(current->children[c] == NULL){
+	current->children[c] = new Nodes();
       }
-      node->add_result(keyword);
+      current = current->children[c];
+    }
+    current->has_word = true;
+  }
+
+  bool common_prefix_search(const string& str){
+    Nodes* current = root;
+
+    for(int i=0;i<str.size();i++){
+      char c = str[i];
+      if(current->children[c] != NULL){
+	current = current->children[c];
+      }
+      else{
+	return false;
+      }
+
+      if(current->has_word) return true;
     }
 
-    vector<AhoCorasick::Node*> nodes;
-    for(int i=0;i<root->get_transitions().size();i++){
-      root->get_transitions()[i]->set_failure(root);
-      vector<AhoCorasick::Node*> tmp = root->get_transitions()[i]->get_transitions();
-      for(int j=0;j<tmp.size();j++){
-	nodes.push_back(tmp[j]);
-      }
-    }
-
-    while(nodes.size() > 0){
-      vector<AhoCorasick::Node*> next_nodes;
-      for(int i=0;i<nodes.size();i++){
-	AhoCorasick::Node* r = nodes[i]->get_parent()->get_failure();
-	char c = nodes[i]->get_char();
-      
-	while((r != NULL) && (r->get_transition(c) != NULL)){
-	  r = r->get_failure();
-	}
-
-	if(r == NULL){
-	  nodes[i]->set_failure(root);
-	}
-	else{
-	  AhoCorasick::Node* tc = r->get_transition(c);
-	  nodes[i]->set_failure(tc);
-	  
-	  set<string> results;
-	  if(tc != NULL) results = tc->get_results();
-	  for(set<string>::iterator it = results.begin();
-	      it != results.end();
-	      it++){
-	    nodes[i]->add_result(*it);
-	  }
-	}
-
-	vector<AhoCorasick::Node*> tmp_nodes;
-	tmp_nodes.reserve(next_nodes.size() + nodes[i]->get_transitions().size()); 
-	merge(next_nodes.begin(), next_nodes.end(),
-	      nodes[i]->get_transitions().begin(), nodes[i]->get_transitions().end(),
-	      back_inserter<vector<AhoCorasick::Node*> >(tmp_nodes));
-	next_nodes.swap(tmp_nodes);
-      }
-
-      nodes = next_nodes;
-    }
-
-    root->set_failure(root);
-    state = root;
-  }
-
-  map<string,int> feed(const string& text,map<string,int> (*callback)(int pos,const string& text)){
-    int index = 0;
-    map<string,int> rv;
-    while(index < text.length()){
-      AhoCorasick::Node* trans = NULL;
-      while(1){
-	trans = state->get_transition(text[index]);
-	if(state == root || trans != NULL) break;
-	state = state->get_failure();
-      }
-
-      if(trans != NULL){
-	state = trans;
-      }
-
-      set<string> results = state->get_results();
-      for(set<string>::iterator it = results.begin();
-	  it != results.end();
-	  it++){
-	rv = (*callback)(index - it->length() + 1,*it);
-	if(!rv.empty()){
-	  return rv;
-	}
-      }
-      index++;
-    }
-    return rv;
+    return false;
   }
 };
 
 class State{
 public:
   int cost;
-  long route;
+  int route;
   int x;
   int y;
-  State(int _x,int _y,int _c,long _r) : cost(_c), route(_r),x(_x),y(_y){}
+  State(char _x,char _y,int _c,int _r) : cost(_c), route(_r),x(_x),y(_y){}
   bool operator<(const State& s) const{
     return cost < s.cost;
   }
@@ -216,27 +100,13 @@ public:
   }
 };
 
-
-map<string,int> handle_first_func(int pos,const string& keyword){
-  map<string,int> total;
-  total[keyword] = pos;
-  return total;
-}
-
-map<string,int> find_first(const string& text,AhoCorasick::SearchMachine* m){
-  map<string,int> (*handle_first)(int pos,const string& keyword) = handle_first_func;
-  map<string,int> rv = m->feed(text,handle_first);
-  return rv;
-}
-
 int main(){
   int H,W;
-
   while(~scanf("%d %d",&H,&W)){
     if(H==0 && W==0) break;
-    set<long> dp[50][50];
+    bitset<500000000> dp[50][50][4];
     char stage[50][50];
-    
+
     int sx = 0;
     int sy = 0;
     int gx = 0;
@@ -259,7 +129,7 @@ int main(){
     int total_prohibited_sequences;
     scanf("%d",&total_prohibited_sequences);
 
-    set<string> keywords;
+    Trie trie;
     for(int sequence_idx=0;
 	sequence_idx < total_prohibited_sequences;
 	sequence_idx++){
@@ -280,12 +150,11 @@ int main(){
 	  num_str += "4";
 	}
       }
+
       reverse(num_str.begin(),num_str.end());
-      keywords.insert(num_str);
+      trie.insert(num_str);
     }
-
-    AhoCorasick::SearchMachine* sm = new AhoCorasick::SearchMachine(keywords);
-
+    
     priority_queue<State,vector<State>,greater<State> > que;
     que.push(State(sx,sy,0,0));
 
@@ -319,16 +188,13 @@ int main(){
 	front = (char)((i+1) + '0');
 	string next =  front + route_str;
 	next = next.substr(0,10);
-	map<string,int> result = find_first(next,sm);
-	if((!result.empty())){
-	  cout << result.begin()->first << endl;
-	  cout << next << endl;
+	if(trie.common_prefix_search(next)){
 	  continue;
 	}
 
-	long next_num = atol(next.c_str());
-	if(dp[dy][dx].count(next_num)) continue;
-	dp[dy][dx].insert(next_num);
+	long next_num = atol(next.substr(0,9).c_str());
+	if(dp[dy][dx][i][next_num]) continue;
+	dp[dy][dx][i][next_num] = true;
 
 	if(stage[dy][dx] == 'G'){
 	  res = cost + 1;
