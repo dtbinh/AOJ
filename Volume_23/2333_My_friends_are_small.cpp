0b5caadcd45d0253937869b4aca10404409bf09e
okaoka <s1220795@u.tsukuba.ac.jp>
2013-12-31 11:22:57 +0900

    solve 2333_My_friends_are_small

--- Volume_23/2333_My_friends_are_small.cpp
+++ Volume_23/2333_My_friends_are_small.cpp
@@ -29,6 +29,7 @@ const static int tx[] = {0,1,1,1,0,-1,-1,-1};
 const static int ty[] = {-1,-1,0,1,1,1,0,-1};
  
 static const double EPS = 1e-12;
+static const int MOD = 1000000007;
 
 int dp[201][10001];
 
@@ -37,25 +38,39 @@ int main(){
   int weight_limit;
   while(~scanf("%d %d",&total_friends,&weight_limit)){
     memset(dp,0,sizeof(dp));
-    int friends[200];
+    int friends[201];
+    int remaining[201];
+    memset(friends,0,sizeof(friends));
+    memset(remaining,0,sizeof(remaining));
     for(int friend_idx = 0; friend_idx < total_friends; friend_idx++){
       scanf("%d",friends + friend_idx);
     }
-    
+    sort(friends,friends + total_friends,greater<int>());
+
+    remaining[total_friends] = 0;
+    for(int i=total_friends-1;i>=0;i--){
+      remaining[i] = remaining[i+1] + friends[i];
+    }
+
+    dp[0][0] = 1;    
     for(int i=0;i<total_friends;i++){
-      dp[i][0] = 1;
       for(int w = 0; w <= 10000; w++){
+	if(w + remaining[i] > weight_limit){
+	  dp[i+1][w] += dp[i][w] % MOD;
+	  dp[i+1][w] %= MOD;
+	}
+
 	if(w + friends[i] > 10000) continue;
-	dp[i+1][w] += dp[i][w];
-	dp[i+1][w + friends[i]] += dp[i][w];
+	dp[i+1][w + friends[i]] += dp[i][w] % MOD;
+	dp[i+1][w + friends[i]] %= MOD;
       }
     }
 
-    for(int w = 0; w <= 10000; w++){
-      for(int i=0;i<=total_friends;i++){
-	  if(dp[i][w] == 0) continue;
-	  printf("i:%d w:%d %d\n",i,w,dp[i][w]);
-      }
+    int res = 0;
+    for(int w = 0; w<= weight_limit; w++){
+      res += dp[total_friends][w] % MOD;
+      res %= MOD;
     }
+    printf("%d\n",res);
   }
 }
