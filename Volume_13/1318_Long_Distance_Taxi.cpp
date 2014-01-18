f9bbd01ac39343ce5060bb06a5e61920fb68bf60
okaoka <s1220795@u.tsukuba.ac.jp>
2014-01-18 10:28:40 +0900

    solved state

--- Volume_13/1318_Long_Distance_Taxi.cpp
+++ Volume_13/1318_Long_Distance_Taxi.cpp
@@ -1,5 +1,6 @@
 #define _USE_MATH_DEFINES
 #define INF 0x3f3f3f3f
+
 #include <cstdio>
 #include <iostream>
 #include <sstream>
@@ -25,15 +26,22 @@ typedef long long ll;
 typedef pair <int,int> P;
 typedef pair <int,P > PP;
 
-map<string,int> cities;
-int dp[301][3001]; //dp[city_id][consumed_LPG] = dist;
-int nodes[3001][3001];
-bool is_LPG[3001];
+class Edge {
+public:
+  int from;
+  int to;
+  int dist;
+  Edge() {}
+  Edge(int _f,int _t,int _d) : from(_f),to(_t),dist(_d){}
+};
 
-int get_city_id(const string& city){
+bool is_LPG[3005];
+bool dp[3005][2005]; //dp[city_id][remaining_LPG] = dist;
+static int gCity_idx = 0;
+
+int get_city_id(map<string,int>& cities,const string& city){
   if(cities.find(city) == cities.end()){
-    int idx = cities.size();
-    cities[city] = idx;
+    cities[city] = gCity_idx++;
   }
   return cities[city];
 }
@@ -42,11 +50,11 @@ class State {
 public:
   int city_id;
   int dist;
-  int consumed_LPG;
+  int remaining_LPG;
   State(int _city_id,int _dist,int _con){
     city_id = _city_id;
     dist = _dist;
-    consumed_LPG = _con;
+    remaining_LPG = _con;
   }
 
   bool operator <(const State& s) const{
@@ -62,67 +70,75 @@ int main(){
   int total_roads;
   int total_LPG_stations;
   int capacity;
-  while(~scanf("%d %d %d",
-	       &total_roads,
-	       &total_LPG_stations,
-	       &capacity)){
+  while(cin >> total_roads >> total_LPG_stations >>capacity){
     if(total_roads == 0
        && total_LPG_stations == 0
        && capacity == 0) break;
+    gCity_idx = 0;
 
-    cities.clear();
-
-    memset(nodes,0x3f,sizeof(nodes));
-    memset(dp,0x3f,sizeof(dp));
+    map<string,int> cities;
+    vector<Edge> edges(total_roads);
     memset(is_LPG,false,sizeof(is_LPG));
+    memset(dp,false,sizeof(dp));
 
     string src,dst;
     cin >> src >> dst;
-    get_city_id(src);
-    get_city_id(dst);
+    get_city_id(cities,src);
+    get_city_id(cities,dst);
 
     for(int road_idx = 0; road_idx < total_roads; road_idx++){
       string from_str,to_str;
       int dist;
 
       cin >> from_str >> to_str >> dist;
-      int from_num = get_city_id(from_str);
-      int to_num = get_city_id(to_str);
-      nodes[from_num][to_num] = dist;
-      nodes[to_num][from_num] = dist;
+      int from_num = get_city_id(cities,from_str);
+      int to_num = get_city_id(cities,to_str);
+
+      edges[road_idx] = Edge(from_num,to_num,dist);
     }
 
     for(int LPG_idx = 0; LPG_idx < total_LPG_stations; LPG_idx++){
       string city;
       cin >> city;
-      is_LPG[get_city_id(city)] = true;
+      is_LPG[get_city_id(cities,city)] = true;
     }
 
     priority_queue<State,vector<State>, greater<State> > que;
-    que.push(State(get_city_id(src),0,0));
-    dp[get_city_id(src)][0] = 0;
+    que.push(State(get_city_id(cities,src),0,10*capacity));
 
+    int dst_id = get_city_id(cities,dst);
+
+    vector<vector<Edge> > nodes(cities.size());
+    for(int i=0; i<edges.size(); i++){
+      Edge e = edges[i];
+      nodes[e.from].push_back(Edge(e.from, e.to, e.dist));
+      nodes[e.to].push_back(Edge(e.to, e.from, e.dist));
+    }
+
+    int res = INF;
     while(!que.empty()){
       State s = que.top();
       que.pop();
-      for(int to=0;to<cities.size();to++){
-	if(nodes[s.city_id][to] >= INF) continue;
-	if(capacity*10 < s.consumed_LPG + nodes[s.city_id][to]) continue;
-	int next_consumed_LPG = (is_LPG[to] ? 0 : (s.consumed_LPG + nodes[s.city_id][to]));
 
-	if(dp[to][next_consumed_LPG]
-	   <= s.dist + nodes[s.city_id][to]) continue;
+      if(s.city_id == dst_id){
+	res = s.dist;
+	goto found;
+      }
+      if(dp[s.city_id][s.remaining_LPG]) continue;
+      dp[s.city_id][s.remaining_LPG] = true;
 
-	dp[to][next_consumed_LPG] = s.dist + nodes[s.city_id][to];
-	que.push(State(to,s.dist + nodes[s.city_id][to],next_consumed_LPG));
+      for(int to_idx=0;to_idx<nodes[s.city_id].size();to_idx++){
+	int to = nodes[s.city_id][to_idx].to;
+	int dist = nodes[s.city_id][to_idx].dist;
+	if(s.remaining_LPG < dist) continue;
+
+	int next_remaining_LPG = (is_LPG[to] ? 10 * capacity : s.remaining_LPG - dist);
+
+	que.push(State(to,s.dist + dist,next_remaining_LPG));
       }
     }
-
-    int dst_id = get_city_id(dst);
-    int res = INF;
-    for(int consumed_LPG = 0;consumed_LPG <= 3000; consumed_LPG++){
-      res = min(dp[dst_id][consumed_LPG],res);
-    }
+  found:;
     printf("%d\n",res >= INF ? -1 : res);
   }
+  return 0;
 }
