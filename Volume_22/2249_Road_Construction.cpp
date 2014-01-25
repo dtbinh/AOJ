9ebb6149a3eefc2ccb355cf75b55341e07a1f126
okaoka <s1220795@u.tsukuba.ac.jp>
2014-01-25 15:25:48 +0900

    sovle 2249_Road_Construction

--- Volume_22/2249_Road_Construction.cpp
+++ Volume_22/2249_Road_Construction.cpp
@@ -33,12 +33,12 @@ static const double EPS = 1e-8;
 
 class Edge{
 public:
-  int from;
   int to;
   int distance;
   int cost;
-  Edge(int _f,int _t,int _d,int _c) : from(_f),to(_t),distance(_d),cost(_c) {}
-  Edge() : from(0), to(0), distance(0), cost(0) {}
+  int road_idx;
+  Edge(int _t,int _d,int _c,int _r) : to(_t),distance(_d),cost(_c),road_idx(_r) {}
+  Edge() {}
   bool operator<(const Edge& e) const{
     return distance < e.distance;
   }
@@ -48,57 +48,18 @@ public:
   }
 };
 
-class UnionFindTree {
-private:
-  int* par;
-  int* rank;
-  int* weight;
+class State {
 public:
-  UnionFindTree(int n){
-    par = new int[n]();
-    rank = new int[n]();
-    weight = new int[n]();
-    for(int i=0;i<n;i++){
-      par[i] = i;
-      rank[i] = 0;
-      weight[i] = 1;
-    }
+  int city;
+  int total_distance;
+  int total_cost;
+  int prev_road;
+  State(int _c,int _td,int _tc,int _pr) : city(_c), total_distance(_td), total_cost(_tc),prev_road(_pr) {}
+  bool operator<(const State& s) const{
+    return total_distance < s.total_distance;
   }
-
-  ~UnionFindTree(){
-    delete[] rank;
-    delete[] par;
-    delete[] weight;
-  }
-
-  int find(int x){
-    if(par[x] == x){
-      return x;
-    } else {
-      return par[x] = find(par[x]);
-    }
-  }
-
-  void unite(int x,int y){
-    x = find(x);
-    y = find(y);
-    if (x == y) return;
-
-    if(rank[x] < rank[y]){
-      par[x] = y;
-      weight[y] += weight[x];
-    } else {
-      par[y] = x;
-      weight[x] += weight[y];
-      if (rank[x] == rank[y]) rank[x]++;
-    }
-  }
-
-  int count(int x){
-    return weight[find(x)];
-  }
-  bool same(int x,int y){
-    return find(x) == find(y);
+  bool operator>(const State& s) const{
+    return total_distance > s.total_distance;
   }
 };
 
@@ -108,25 +69,55 @@ int main(){
   while(~scanf("%d %d",&total_cities,&total_roads)){
     if(total_cities == 0 &&  total_roads == 0) break;
 
-    UnionFindTree uft(20001);
-    vector<Edge> edges;
+    vector<Edge> edges[20001];
 
-    priority_queue<Edge> que;
+    int road_cost[20001];
+    memset(road_cost,0,sizeof(road_cost));
+
     for(int road_idx = 0; road_idx < total_roads; road_idx++){
       int from,to,distance,cost;
       scanf("%d %d %d %d",&from,&to,&distance,&cost);
-      que.push(Edge(from,to,distance,cost));
+      edges[from-1].push_back(Edge(to-1,distance,cost,road_idx));
+      edges[to-1].push_back(Edge(from-1,distance,cost,road_idx));
+      road_cost[road_idx] = cost;
+    }
+    
+    int dp[20001];
+    memset(dp,0x3f,sizeof(dp));
+
+    priority_queue<State,vector<State>,greater<State> > que;
+    que.push(State(0,0,0,-1));
+    
+    while(!que.empty()){
+      State s = que.top();
+      que.pop();
+
+      if(dp[s.city] <= s.total_distance) continue;
+      dp[s.city] = s.total_distance;
+
+      for(int i=0;i<edges[s.city].size();i++){
+	int to = edges[s.city][i].to;
+	int next_distance = edges[s.city][i].distance;
+	int next_cost = edges[s.city][i].cost;
+
+	que.push(State(to,s.total_distance + next_distance,s.total_cost + next_cost,i));
+      }
     }
 
     int res = 0;
-    while(!que.empty()){
-      Edge e = que.top();
-      que.pop();
-      if(!uft.same(e.from,e.to)){
-	uft.unite(e.from,e.to);
-	printf("unite %d %d\n",e.from,e.to);
-	res += e.cost;
+    for(int to=1; to<total_cities; to++){
+      int min_v = INF;
+      for(int from_idx=0; from_idx<edges[to].size(); from_idx++){
+	int from = edges[to][from_idx].to;
+	int distance = edges[to][from_idx].distance;
+	int cost = edges[to][from_idx].cost;
+	
+	//check whether the path is corresponding to the dijkstra's shortest path or not
+	if(dp[from] + distance == dp[to] && cost < min_v){
+	  min_v = cost;
+	}
       }
+      res += min_v;
     }
 
     printf("%d\n",res);
