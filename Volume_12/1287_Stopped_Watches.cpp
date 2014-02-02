60da6ac2354fca0f2fb0ea9404e3505ac6e7bb6b
okaoka <s1220795@u.tsukuba.ac.jp>
2014-02-02 23:29:51 +0900

    fix check function

--- Volume_12/1287_Stopped_Watches.cpp
+++ Volume_12/1287_Stopped_Watches.cpp
@@ -31,6 +31,36 @@ static const double EPS = 1e-8;
 static const int tx[] = {0,1,0,-1};
 static const int ty[] = {-1,0,1,0};
 
+bool check(int hands[3]){
+  for(int h=0;h<12;h++){
+    for(int m=0;m<60;m++){
+      for(int s=0;s<60;s++){
+	int h_angle = h*30 + (m/12)*6;
+	int m_angle = m*6;
+	int s_angle = s*6;
+
+	int idx[] = {0,1,2};
+	do{
+	  int a = hands[idx[0]];
+	  int b = hands[idx[1]];
+	  int c = hands[idx[2]];
+
+	  for(int offset=0;offset<360;offset+=6){
+	    int next_a = ((a*6 + offset) % 360);
+	    int next_b = ((b*6 + offset) % 360);
+	    int next_c = ((c*6 + offset) % 360);
+
+	    if(next_a == h_angle 
+	       && next_b == m_angle 
+	       && next_c == s_angle){
+	      printf("h:%d m:%d s:%d\n",h,m,s);
+	    }
+	  }
+	}while(next_permutation(idx,idx+3));
+      }
+    }
+  }
+}
 int main(){
   int total_watches;
   while(~scanf("%d",&total_watches)){
@@ -40,6 +70,7 @@ int main(){
       for(int hand_idx = 0; hand_idx < 3; hand_idx++){
 	scanf("%d",hands + hand_idx);
       }
+      check(hands);
     }
   }
 }
