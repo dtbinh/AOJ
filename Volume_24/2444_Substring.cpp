9985dc4d40513dcb63c07e4b0d033e30fdef1b9d
okaoka <s1220795@u.tsukuba.ac.jp>
2014-02-04 01:15:34 +0900

    solve 2444_Substring

--- Volume_24/2444_Substring.cpp
+++ Volume_24/2444_Substring.cpp
@@ -37,13 +37,25 @@ int main(){
   while(~scanf("%d %d",&string_size,&total_queries)){
     string str;
     cin >> str;
-    int left[300000];
-    int right[300000];
+    
+    unsigned long long B = 100000007ULL;
+    unsigned long long h[300001];
+    unsigned long long t[300001];
+    h[0] = 0;
+    t[0] = 1;
+    for(int i=1; i <= str.size(); i++){
+      t[i] = t[i-1] * B;
+      h[i] = h[i-1] * B + str[i - 1];
+    }
+
+    int left[300001];
+    int right[300001];
     memset(left,0,sizeof(left));
     memset(right,0,sizeof(right));
     left[0] = 1;
     right[0] = 1;
-    set<string> visited;
+    set<unsigned long long> visited;
+
     for(int query_idx = 1; query_idx <= total_queries; query_idx++){
       string query;
       cin >> query;
@@ -64,7 +76,7 @@ int main(){
 	right[query_idx]=right[query_idx-1] + 1;
       }
 
-      visited.insert(str.substr(left[query_idx]-1,right[query_idx]-left[query_idx]+1));
+      visited.insert(h[right[query_idx]] - h[left[query_idx] - 1] * t[right[query_idx] - left[query_idx] + 1]);
     }
     printf("%d\n",visited.size());
   }
