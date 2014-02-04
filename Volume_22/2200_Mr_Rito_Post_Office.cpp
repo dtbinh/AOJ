7d3005d05c49a28bc7146e603179a55370dc3b09
okaoka <s1220795@u.tsukuba.ac.jp>
2014-02-04 23:36:57 +0900

    add 2200_Mr_Rito_Post_Office

---
+++ Volume_22/2200_Mr_Rito_Post_Office.cpp
@@ -0,0 +1,47 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+#include <iterator>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {0,1,0,-1};//URDL
+int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int total_cities;
+  int total_roads;
+  for(int road_idx = 0; road_idx < total_roads; road_idx++){
+    int src,dst,time;
+    char type[2];
+    scanf("%d %d %d %s",&src,&dst,&time,type);
+  }
+  int total_routes;
+  for(int route_idx = 0; route_idx < total_routes; route_idx++){
+    int route;
+    scanf("%d",&route);
+  }
+}
