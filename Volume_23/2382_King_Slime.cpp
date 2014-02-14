0a3e5f2eb18b3380181e255d7b58f2db301c5d83
okaoka <s1220795@u.tsukuba.ac.jp>
2014-02-14 22:50:27 +0900

    add 2382_King_Slime

---
+++ Volume_23/2382_King_Slime.cpp
@@ -0,0 +1,44 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+const static int tx[] = {0,1,1,1,0,-1,-1,-1};
+const static int ty[] = {-1,-1,0,1,1,1,0,-1};
+ 
+static const double EPS = 1e-12;
+
+int main(){
+  int total_slimes;
+  int W,H;
+  while(~scanf("%d %d %d",&total_slimes,&W,&H)){
+    for(int slime_idx=0; slime_idx<total_slimes; slime_idx++){
+      int x,y;
+      scanf("%d %d",&x,&y);
+      x--;
+      y--;
+    }
+  }
+}
