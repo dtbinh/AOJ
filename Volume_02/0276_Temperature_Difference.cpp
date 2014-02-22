c4caaaf8a197a81a5bc5add7dfd1552b719c1354
okaoka <s1220795@u.tsukuba.ac.jp>
2014-02-23 00:23:24 +0900

    solve 0276_Temperature_Difference

---
+++ Volume_02/0276_Temperature_Difference.cpp
@@ -0,0 +1,31 @@
+#define _USE_MATH_DEFINES
+#define INF 10000000
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,string> P;
+
+static const double eps = 1e-8;
+
+int main(){
+  int a,b;
+  while(~scanf("%d %d",&a,&b)){
+    printf("%d\n",a-b);
+  }
+}
