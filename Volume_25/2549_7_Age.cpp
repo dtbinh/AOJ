fe0bbe454aa7c40164df63c7b0e95cb4d6c1171a
okaoka <s1220795@u.tsukuba.ac.jp>
2014-02-22 19:18:03 +0900

    answer is correct

--- Volume_25/2549_7_Age.cpp
+++ Volume_25/2549_7_Age.cpp
@@ -39,10 +39,10 @@ public:
   Star(int _l,int _u,int _i) : lower_bound_time(_l),upper_bound_time(_u),idx(_i){}
   Star() : lower_bound_time(0),upper_bound_time(0){}
   bool operator<(const Star& s) const{
-    return lower_bound_time < s.lower_bound_time;
+    return upper_bound_time < s.upper_bound_time;
   }
   bool operator>(const Star& s) const{
-    return lower_bound_time > s.lower_bound_time;
+    return upper_bound_time > s.upper_bound_time;
   }
 };
 
@@ -100,27 +100,31 @@ int main(){
 
 
     int max_duration[501];
-    memset(max_duration,0,sizeof(max_duration));
+    fill(max_duration,max_duration+501,-INF);
 
+    int res = 0;
     start = conv[start];
     for(int to_star_idx = 0; to_star_idx < total_stars; to_star_idx++){
       if(cost[start][to_star_idx] <= stars[to_star_idx].upper_bound_time){
 	max_duration[to_star_idx]
 	  = stars[to_star_idx].upper_bound_time
 	  - max(cost[start][to_star_idx],stars[to_star_idx].lower_bound_time);
+	res = max(res,max_duration[to_star_idx]);
       }
     }
 
-    int res = 0;
+
     for(int from_star_idx = 0; from_star_idx < total_stars; from_star_idx++){
+      if(max_duration[from_star_idx] < 0) continue;
+
       for(int to_star_idx = 0; to_star_idx < total_stars; to_star_idx++){
 	int time = stars[from_star_idx].upper_bound_time + cost[from_star_idx][to_star_idx];
-	if(time >= stars[to_star_idx].upper_bound_time) continue;
+	if(time > stars[to_star_idx].upper_bound_time) continue;
 
 	if(max_duration[to_star_idx] 
-	   < max_duration[from_star_idx]
-	   + stars[to_star_idx].upper_bound_time - max(time,stars[to_star_idx].lower_bound_time)){
-	  max_duration[to_star_idx] = max_duration[from_star_idx]
+	   < max_duration[from_star_idx] + stars[to_star_idx].upper_bound_time - max(time,stars[to_star_idx].lower_bound_time)){
+	  max_duration[to_star_idx]
+	    = max_duration[from_star_idx]
 	    + stars[to_star_idx].upper_bound_time - max(time,stars[to_star_idx].lower_bound_time);
 	  res = max(res,max_duration[to_star_idx]);
 	}
