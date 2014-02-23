528e6a49be4b0d69f1237afa9872faa7b97c598e
okaoka <s1220795@u.tsukuba.ac.jp>
2014-02-24 00:46:55 +0900

    solve 0281_Formation

--- Volume_02/0281_Formation.cpp
+++ Volume_02/0281_Formation.cpp
@@ -23,68 +23,34 @@ typedef pair<int,string> P;
 
 static const double EPS = 1e-8;
 
-class Coder{
-  int c;
-  int a;
-  int n;
-public:
-  Coder(int _c,int _a,int _n) : c(_c),a(_a),n(_n){}
-  bool operator<(const Coder& _c) const{
-    if(c == _c.c){
-      if(n == _c.n){
-	return a < _c.a;
-      }
-      else if(n != _c.n){
-	return n < _c.n;
-      }
-    }
-    else if(c != _c.c){
-      return c < _c.c;
-    }
-  }
-  bool operator>(const Coder& _c) const{
-    if(c == _c.c){
-      if(n == _c.n){
-	return a > _c.a;
-      }
-      else if(n != _c.n){
-	return n > _c.n;
-      }
-    }
-    else if(c != _c.c){
-      return c > _c.c;
-    }
-  }
-};
-
-map<Coder,int> dp;
-
-int dfs(int c,int a,int n,int count){
-  if(c < 0 || a < 0 || n < 0) return 0;
-  
-  map<Coder,int>::iterator it;
-  if((it = dp.find(Coder(c,a,n))) != dp.end()) return it->second;
-
-  int res = count;
-  //cca
-  res = max(res,dfs(c-2,a-1,n,count+1));
-
-  //ccc
-  res = max(res,dfs(c-3,a,n,count+1));
-
-  //can
-  res = max(res,dfs(c-1,a-1,n-1,count+1));
-  
-  return (dp[Coder(c,a,n)] = res);
-}
-
 int main(){
   int N;
   while(~scanf("%d",&N)){
+
     for(int i=0;i<N;i++){
       int c,a,n;
+
+      int count = 0;
       scanf("%d %d %d",&c,&a,&n);
-      printf("%d\n",dfs(c,a,n,0));
+      
+      while(c >= 1 && a >= 1 && n >= 1){
+	c--;
+	a--;
+	n--;
+	count++;
+      }
+
+      while(a >= 1 && c >= 2){
+	a--;
+	c -= 2;
+	count++;
+      }
+
+      while(c >= 3){
+	  c -= 3;
+	  count++;
+      }
+      printf("%d\n",count);
     }
   }
 }
