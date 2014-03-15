cb4e2f857e433adb14b7c0b016d0dfb99e4809f1
okaoka <s1220795@u.tsukuba.ac.jp>
2014-03-16 00:54:22 +0900

    solve 0540_Amidakuji

--- Volume_05/0540_Amidakuji.cpp
+++ Volume_05/0540_Amidakuji.cpp
@@ -24,7 +24,6 @@ using namespace std;
   
 typedef long long ll;
 typedef pair <int,int> P;
-typedef pair <int,P> PP;
   
 static const double EPS = 1e-8;
   
@@ -46,7 +45,12 @@ int main(){
 	       &total_horizontal_bars,
 	       &max_height,
 	       &total_selected_bars)){
-    ll sum = 0;
+    if(total_vertical_bars == 0
+       && total_horizontal_bars == 0
+       && max_height == 0
+       && total_selected_bars == 0) break;
+
+    int sum = 0;
     int points[1001];
     memset(points,0,sizeof(points));
     for(int i=0;i<total_vertical_bars;i++){
@@ -63,26 +67,55 @@ int main(){
     int matrix[1001];
     for(int i=0;i<total_vertical_bars;i++) matrix[i] = i;
 
-    int mod[1001][1001];
+    vector<P> change;
+    vector<P> log;
+
     for(int height=0;height < max_height;height++){
       for(int i=0;i<bars[height].size();i++){
 	int x = bars[height][i];
+	change.push_back(P(matrix[x],matrix[x+1]));
 	swap(matrix[x],matrix[x+1]);
       }
-
-      memcpy(mod[height],matrix,sizeof(int)*1001);
     }
 
-    //re-init
-    for(int i=0;i<total_vertical_bars;i++) matrix[i] = i;
-    for(int height=0;height < max_height;height++){
-      for(int i=0;i<bars[height].size();i++){
-	mod[height-1];
 
-	mod[height+1];
-	mod[max_height];
+    for(int height=max_height-1;height >= 0;height--){
+      for(int i=bars[height].size()-1;i >= 0;i--){
+	int x = bars[height][i];
+	log.push_back(P(points[x],points[x+1]));
+	swap(points[x],points[x+1]);
       }
     }
 
+    reverse(log.begin(),log.end());
+
+    int res = 0;
+    for(int i=0;i<total_selected_bars;i++){
+      res += points[i];
+    }
+
+    int base = res;
+    int idx = 0;
+    for(int height=0;height < max_height;height++){
+      for(int i=0;i<bars[height].size();i++){
+	if(change[idx].first >= total_selected_bars
+	   && change[idx].second < total_selected_bars){
+	  res = min(res,
+		    base
+		    - log[idx].first
+		    + log[idx].second);
+	}
+	
+	if(change[idx].first < total_selected_bars
+	   && change[idx].second >= total_selected_bars){
+	  res = min(res,
+		    base
+		    + log[idx].first
+		    - log[idx].second);
+	}
+	idx++;
+      }
+    }
+    printf("%d\n",res);
   }
 }
