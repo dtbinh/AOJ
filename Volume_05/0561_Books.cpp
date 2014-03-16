16ca90247972026f7ed7feaf4e58ec54320daad1
okaoka <s1220795@u.tsukuba.ac.jp>
2014-03-16 22:42:14 +0900

    solve 0561_Books

--- Volume_05/0561_Books.cpp
+++ Volume_05/0561_Books.cpp
@@ -37,8 +37,31 @@ int main(){
   while(~scanf("%d %d",
 	       &total_books,
 	       &total_sell_books)){
+    vector<int> books[11];
     for(int book_idx=0;book_idx<total_books;book_idx++){
+      int price,category;
       scanf("%d %d",&price,&category);
+      books[category].push_back(price);
     }
+
+    for(int category=1;category<=10;category++){
+      sort(books[category].begin(),books[category].end(),greater<int>());
+    }
+
+    int dp[2001];
+    memset(dp,0,sizeof(dp));
+    for(int category=1;category<=10;category++){
+      for(int sell=total_sell_books;sell>=0;sell--){
+	int sum = 0;
+	for(int book_idx=0;book_idx<books[category].size();book_idx++){
+	  if(sell + (book_idx+1) > total_sell_books) continue;
+	  int price = books[category][book_idx];
+	  sum += price;
+	  dp[sell+(book_idx+1)] = max(dp[sell] + sum + (book_idx+1)*book_idx,
+				      dp[sell+(book_idx+1)]);
+	}
+      }
+    }
+    printf("%d\n",dp[total_sell_books]);
   }
 }
