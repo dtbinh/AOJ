6c89b1c31124130b52649a9bd0840bef84f176b7
okaoka <s1220795@u.tsukuba.ac.jp>
2014-04-02 20:51:30 +0900

    solve 2578_Disciple_Life_is_Hard

---
+++ Volume_25/2578_Disciple_Life_is_Hard.cpp
@@ -0,0 +1,58 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+  
+int tx[] = {0,1,0,-1};
+int ty[] = {-1,0,1,0};
+
+int main(){
+  //STUNOD
+  int initital_HP;
+  int total_trainings;
+  int training_norma;
+  int total_donuts;
+  int recover;
+  int total_days;
+  while(~scanf("%d %d %d %d %d",
+	       &initital_HP,
+	       &total_trainings,
+	       &training_norma,
+	       &total_donuts,
+	       &recover,
+	       &total_days)){
+    for(int training_idx=0;training_idx<total_trainings;training_idx++){
+      int training_cost,consume_cal;
+      scanf("%d %d",&training_cost,&consume_cal);
+    }
+    for(int donuts=0;donuts<total_donuts;donuts_idx++){
+      int well_being_level,retain_cal;
+      scanf("%d %d",&well_being_level,&retain_cal);
+    }
+  }
+}
