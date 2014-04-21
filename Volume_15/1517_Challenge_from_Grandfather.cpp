8cdbe9ea690b985061fd7c8fc7d1e5c4594afb99
okaoka <mnfrf593@yahoo.co.jp>
2014-04-22 07:17:09 +0900

    add offset

--- Volume_15/1517_Challenge_from_Grandfather.cpp
+++ Volume_15/1517_Challenge_from_Grandfather.cpp
@@ -40,8 +40,39 @@ void rotate(int r,int c,int size,int angle){
       tmp[y-r][x-c] = matrix[y][x];
     }
   }
-
+  for(int y=r;y<r+size;y++){
+    for(int x=c;x<c+size;x++){
+      printf("%d ",tmp[y-r][x-c]);
+    }
+    printf("\n");
+  }
   
+  int offset_y = (angle == 90 || angle == 180) ? size - 1 : 0;
+  int offset_x = (angle == 180 || angle == 270) ? size - 1 : 0;
+  
+  int next[size+1][size+1];
+  for(int y=0;y<size;y++){
+    for(int x=0;x<size;x++){
+      double dx = x * cos((double)angle * M_PI/180.0) + y * sin((double)angle * M_PI/180.0) + offset_x;
+      double dy = - x * sin((double)angle * M_PI/180.0) + y * cos((double)angle * M_PI/180.0) + offset_y;
+      printf("x:%d y:%d\n",x,y);
+      printf("dx:%d dy:%d\n",(int)(dx + (dx < 0 ? - EPS : EPS)),(int)(dy + (dy < 0 ? - EPS : EPS)));
+      next[(int)(dy + (dy < 0 ? - EPS : EPS))][(int)(dx + (dx < 0 ? - EPS : EPS))] = tmp[y][x];
+    }
+  }
+
+  for(int y=r;y<r+size;y++){
+    for(int x=c;x<c+size;x++){
+      printf("%d ",next[y-r][x-c]);
+    }
+    printf("\n");
+  }
+
+  for(int y=r;y<r+size;y++){
+    for(int x=c;x<c+size;x++){
+      matrix[y][x] = next[y-r][x-c];
+    }
+  }
 }
 
 void reversal(int r,int c,int size){
@@ -107,27 +138,34 @@ int main(){
       switch(type){
       case 0:
 	scanf("%d %d %d %d",&r,&c,&angle,&size);
-	rotate(r,c,angle,size);
+	rotate(r-1,c-1,angle,size);
 	break;
       case 1:
 	scanf("%d %d %d %d",&r,&c,&size);
-	reversal(r,c,size);
+	reversal(r-1,c-1,size);
 	break;
       case 2:
 	scanf("%d",&r);
-	left_shift(r);
+	left_shift(r-1);
 	break;
       case 3:
 	scanf("%d",&r);
-	right_shift(r);
+	right_shift(r-1);
 	break;
       case 4:
 	scanf("%d %d",&r,&c);
-	island_reversal(r,c);
+	island_reversal(r-1,c-1);
 	break;
       default:
 	break;
       }
+    }
+
+    for(int y=0;y<matrix_size;y++){
+      for(int x=0;x<matrix_size;x++){
+	printf("%s%d",x == 0 ? "" : " ",matrix[y][x]);
+      }
+      printf("\n");
     }
   }
 }
