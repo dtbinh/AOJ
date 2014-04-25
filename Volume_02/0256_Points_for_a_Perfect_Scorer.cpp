614cfd3f76cdb7fab50fb9d1d5c9686720acf44a
okaoka <mnfrf593@yahoo.co.jp>
2014-04-26 01:59:46 +0900

    solve 0256_Points_for_a_Perfect_Scorer

---
+++ Volume_02/0256_Points_for_a_Perfect_Scorer.cpp
@@ -0,0 +1,34 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,string> P;
+
+static const double EPS = 1e-8;
+
+int main(){
+  int sum = 0;
+  for(int i=0;i<10;i++){
+    int tmp;
+    scanf("%d",&tmp);
+    sum += tmp;
+  }
+  printf("%d\n",sum);
+}
