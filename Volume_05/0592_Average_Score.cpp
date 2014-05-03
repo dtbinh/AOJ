315ca669445b526040b7479af146fa1f226fd05a
okaoka <mnfrf593@yahoo.co.jp>
2014-05-03 23:21:43 +0900

    solve 0592_Average_Score

---
+++ Volume_05/0592_Average_Score.cpp
@@ -0,0 +1,43 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+  
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+
+int main(){
+
+  int sum = 0;
+  for(int i=0;i<5;i++){
+    int score;
+    scanf("%d",&score);
+    sum += max(40,score);
+  }
+  printf("%d\n",sum / 5);
+}
