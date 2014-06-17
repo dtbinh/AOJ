622834402779f8ccff6ae793d44833f22bb633b0
okaoka <mnfrf593@yahoo.co.jp>
2014-06-17 22:56:34 +0900

    I should give up bidirectional search(A-star)

--- Volume_13/1339_Dragon's_Cruller.cpp
+++ Volume_13/1339_Dragon's_Cruller.cpp
@@ -39,8 +39,9 @@ public:
   string stage;
   int cost;
   int heuristic;
-  State(int _w,const string& _st,int _c,int _h)
-    : white_space_pos(_w),stage(_st),cost(_c),heuristic(_h) {}
+  short depth;
+  State(int _w,const string& _st,int _c,int _h,int _d)
+    : white_space_pos(_w),stage(_st),cost(_c),heuristic(_h),depth(_d) {}
   bool operator <(const State& s) const {
     return cost + heuristic < s.cost + s.heuristic;
   }
@@ -60,16 +61,19 @@ int calc(const string& stage,char conv[256],
   return res;
 }
 
-void bfs(int white_space_pos){
-  set<string> dp;
-  char conv[256];
+void bfs(int white_space_pos_s,int white_space_pos_g){
+  map<string,int> s2g_dp;
+  map<string,int> g2s_dp;
+  char conv_g[256];
+  char conv_s[256];
 
   int edges[10][10];
   memset(edges,0x3f,sizeof(edges));
 
 
   for(int i=0;i<9;i++){
-    conv[goal[i]] = i;
+    conv_g[goal[i]] = i;
+    conv_g[init[i]] = i;
     for(int j=0;j<4;j++){
       edges[i][move[i][j]] = move_cost[j % 2];
       edges[move[i][j]][i] = move_cost[j % 2];
@@ -85,48 +89,112 @@ void bfs(int white_space_pos){
     }
   }
 
-
   priority_queue<State,vector<State>,greater<State> > que;
 
-  que.push(State(white_space_pos,init,0,calc(init,conv,edges)));
+  que.push(State(white_space_pos_s,init,0,calc(init,conv_s,edges),0));
 
-  int res = 0;
+  map<string,int>::iterator it;
+  int res = INF;
   while(!que.empty()){
     State s = que.top();
     que.pop();
-    dp.insert(s.stage);
+
+    if(s.depth >= 11) continue;
+    s2g_dp[s.stage] = s.cost;
 
     if(s.stage == goal){
       res = s.cost;
       break;
     }
 
-    int h = calc(s.stage,conv,edges);
+    // int h = calc(s.stage,conv_s,edges);
+    int h = 0;
     for(int i=0;i<4;i++){
 
       int from = move[s.white_space_pos][i];
       string next = s.stage;
 
-      int gpos_sp = conv[s.stage[s.white_space_pos]];
-      h -= edges[s.white_space_pos][gpos_sp];
+      // int gpos_sp = conv_s[s.stage[s.white_space_pos]];
+      // h -= edges[s.white_space_pos][gpos_sp];
 
-      int gpos_nsp = conv[s.stage[from]];
-      h -= edges[from][gpos_nsp];
+      // int gpos_nsp = conv_s[s.stage[from]];
+      // h -= edges[from][gpos_nsp];
 
       next[from] = '0';
       next[s.white_space_pos] = s.stage[from];
 
-      gpos_sp = conv[next[from]];
-      h += edges[from][gpos_sp];
+      // gpos_sp = conv_s[next[from]];
+      // h += edges[from][gpos_sp];
 
-      gpos_nsp = conv[next[s.white_space_pos]];
-      h += edges[s.white_space_pos][gpos_nsp];
+      // gpos_nsp = conv_s[next[s.white_space_pos]];
+      // h += edges[s.white_space_pos][gpos_nsp];
 
-      map<string,int>::iterator it;
-      if(dp.count(next)){
+      if(s2g_dp.find(next) != s2g_dp.end()
+	 && s2g_dp[next] <= s.cost + move_cost[i % 2]){
 	continue;
       }
-      que.push(State(from,next,s.cost + move_cost[i % 2],h));
+      s2g_dp[next] = s.cost + move_cost[i % 2];
+      que.push(State(from,next,s.cost + move_cost[i % 2],h,s.depth+1));
+    }
+  }
+
+  priority_queue<State,vector<State>,greater<State> > empty;
+  swap(que,empty);
+
+  priority_queue<State,vector<State>,greater<State> > que2;
+  que2.push(State(white_space_pos_g,goal,0,calc(goal,conv_g,edges),0));
+
+  while(!que2.empty()){
+    State s = que2.top();
+    que2.pop();
+
+    if(s.depth >= 11) continue;
+    g2s_dp[s.stage] = s.cost;
+
+    if(s.stage == init){
+      res = s.cost;
+      break;
+    }
+
+    // int h = calc(s.stage,conv_g,edges);
+    int h = 0;
+    for(int i=0;i<4;i++){
+
+      int from = move[s.white_space_pos][i];
+      string next = s.stage;
+
+      // int gpos_sp = conv_g[s.stage[s.white_space_pos]];
+      // h -= edges[s.white_space_pos][gpos_sp];
+
+      // int gpos_nsp = conv_g[s.stage[from]];
+      // h -= edges[from][gpos_nsp];
+
+      next[from] = '0';
+      next[s.white_space_pos] = s.stage[from];
+
+      // gpos_sp = conv_g[next[from]];
+      // h += edges[from][gpos_sp];
+
+      // gpos_nsp = conv_g[next[s.white_space_pos]];
+      // h += edges[s.white_space_pos][gpos_nsp];
+
+      if(g2s_dp.find(next) != g2s_dp.end()
+	 && g2s_dp[next] <= s.cost + move_cost[i % 2]){
+	continue;
+      }
+      g2s_dp[next] = s.cost + move_cost[i % 2];
+      que2.push(State(from,next,s.cost + move_cost[i % 2],h,s.depth+1));
+    }
+  }
+
+  if(res == INF){
+    for(map<string,int>::iterator it = g2s_dp.begin();
+	it != g2s_dp.end();
+	it++){
+      map<string,int>::iterator it2;
+      if((it2 = s2g_dp.find(it->first)) != s2g_dp.end()){
+	res = min(res,it->second + it2->second);
+      }
     }
   }
   printf("%d\n",res);
@@ -190,24 +258,28 @@ int main(){
     init = "";
     goal = "";
 
-    int white_space_pos = 0;
+    int white_space_pos_s = 0;
     for(int y=0;y<3;y++){
       for(int x=0;x<3;x++){
 	int num = 0;
 	scanf("%d",&num);
 	init.push_back('0' + num);
 	if(num == 0){
-	  white_space_pos = y * W + x;
+	  white_space_pos_s = y * W + x;
 	}
       }
     }
+    int white_space_pos_g = 0;
     for(int y=0;y<3;y++){
       for(int x=0;x<3;x++){
 	int num = 0;
 	scanf("%d",&num);
 	goal.push_back('0' + num);
+	if(num == 0){
+	  white_space_pos_g = y * W + x;
+	}
       }
     }
-    bfs(white_space_pos);
+    bfs(white_space_pos_s,white_space_pos_g);
   }
 }
