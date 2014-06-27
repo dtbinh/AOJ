06c094217276da38b818945ee6f89d3a2024e689
okaoka <mnfrf593@yahoo.co.jp>
2014-06-27 23:03:04 +0900

    change strategy

--- Volume_25/2565_Broken_Audio_Signal.cpp
+++ Volume_25/2565_Broken_Audio_Signal.cpp
@@ -36,95 +36,109 @@ int main(){
     vector<int> signals;
     queue<int> unknown;
 
+    bool has_x = false;
+    bool has_num = false;
     for(int i=0; i < N; i++){
       char signal[12];
       int num;
       scanf("%s",signal);
       if(signal[0] != 'x'){
 	num = atoi(signal);
+	has_num = true;
       }
       else{
 	num = INF;
+	has_x = true;
 	unknown.push(i);
       }
       signals.push_back(num);
     }
     
-    int lower = -INF;
-    int upper = INF;
-
-    while(!unknown.empty()){
-      int idx = unknown.front();
-      unknown.pop();
-      int lhs = idx - 1;
-      int rhs = idx + 1;
+    if(has_num && has_x){
+      //x x 1 x
+      int lower = -INF;
+      int upper = INF;
       
-      //even
-      if((idx+1) % 2 == 0){
-	if(lhs >= 0 && signals[lhs] != INF) {
-	  lower = max(signals[lhs]+1,lower);
-	}
-	if(rhs < signals.size() && signals[rhs] != INF){
-	  lower = max(signals[rhs]+1,lower);
-	}
-      }
-      //odd
-      else{
-	if(rhs < signals.size() && signals[rhs] != INF){
-	  upper = min(signals[rhs]-1,upper);
-	}
-	if(lhs >= 0 && signals[lhs] != INF){
-	  upper = min(signals[lhs]-1,upper);
-	}
-      }
-    }
-
-    bool is_none = false;
-    for(int i=0;i<signals.size();i++){
-      if(signals[i] == INF) continue;
-
-      int lhs = i - 1;
-      int rhs = i + 1;
-      if(lhs >= 0 && signals[lhs] != INF){
-	if((i + 1) % 2 == 0){
-	  if(signals[i] - signals[lhs] < 0){
-	    is_none = true;
-	    break;
+      while(!unknown.empty()){
+	int idx = unknown.front();
+	unknown.pop();
+	int lhs = idx - 1;
+	int rhs = idx + 1;
+	
+	//even
+	if((idx+1) % 2 == 0){
+	  if(lhs >= 0 && signals[lhs] != INF) {
+	    lower = max(signals[lhs]+1,lower);
+	  }
+	  if(rhs < signals.size() && signals[rhs] != INF){
+	    lower = max(signals[rhs]+1,lower);
 	  }
 	}
+	//odd
 	else{
-	  if(signals[i] - signals[lhs] > 0){
-	    is_none = true;
-	    break;
+	  if(rhs < signals.size() && signals[rhs] != INF){
+	    upper = min(signals[rhs]-1,upper);
+	  }
+	  if(lhs >= 0 && signals[lhs] != INF){
+	    upper = min(signals[lhs]-1,upper);
 	  }
 	}
       }
-      if(rhs < signals.size() && signals[rhs] != INF){
-	if((i + 1) % 2 == 0){
-	  if(signals[i] - signals[rhs] < 0){
-	    is_none = true;
-	    break;
-	  }
-	}
-	else{
-	  if(signals[i] - signals[rhs] > 0){
-	    is_none = true;
-	    break;
-	  }
-	}
+
+      if(upper - lower > 0){
+	printf("ambiguous\n");
+      }
+      else if(upper == lower){
+	printf("%d\n",upper);
+      }
+      else if(upper - lower < 0){
+	printf("none\n");
       }
     }
 
-    cout << "upper " << upper << endl;
-    cout << "lower " << lower << endl;
-    if(!is_none && upper - lower > 0){
-      printf("ambiguous\n");
-    }
-    else if(!is_none && upper == lower){
-      printf("%d\n",upper);
-    }
-    else if(is_none || upper - lower < 0){
+    else if(!has_num && has_x){
+      //x x x
       printf("none\n");
+    }
+    else if(has_num && !has_x){
+      //1 2 3
+      
+      bool is_none = false;
+      for(int i=0;i<signals.size();i++){
+	int lhs = i - 1;
+	int rhs = i + 1;
+	if(lhs >= 0 && signals[lhs] != INF){
+	  if((i + 1) % 2 == 0){
+	    if(signals[i] - signals[lhs] < 0){
+	      is_none = true;
+	      break;
+	    }
+	  }
+	  else{
+	    if(signals[i] - signals[lhs] > 0){
+	      is_none = true;
+	      break;
+	    }
+	  }
+	}
+	if(rhs < signals.size() && signals[rhs] != INF){
+	  if((i + 1) % 2 == 0){
+	    if(signals[i] - signals[rhs] < 0){
+	      is_none = true;
+	      break;
+	    }
+	  }
+	  else{
+	    if(signals[i] - signals[rhs] > 0){
+	      is_none = true;
+	      break;
+	    }
+	  }
+	}
+	
+      }
+     
+      printf("%s\n",is_none ? "none" : "ambiguous");
     }
   }
 }
