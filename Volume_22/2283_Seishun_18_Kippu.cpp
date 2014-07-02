2e43ddc12ddd755a6d85abbcc13b63ebdeeed230
okaoka <mnfrf593@yahoo.co.jp>
2014-07-03 08:34:48 +0900

    add input portion

---
+++ Volume_22/2283_Seishun_18_Kippu.cpp
@@ -0,0 +1,62 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+
+int str2num(const string& station,map<string,int>& dict){
+  if(dict.find(station) == dict.end()){
+    int idx = dict.size();
+    dict[station] = idx;
+    return idx;
+  }
+  return dict[station];
+}
+
+int main(){
+  int total_stations;
+  int total_lines;
+  while(~scanf("%d %d",
+	       &total_stations,
+	       &total_lines)){
+    map<string,int> dist;
+    string shirokuro_location;
+    string R2d_location;
+    string Aizu_location;
+
+    for(int line_idx = 0; line_idx < total_lines; line_idx++){
+      string from,to;
+      int distance;
+      int delay;
+      cin >> from >> to >> distance >> delay;
+      int cost = distance / 40 + delay;
+    }
+  }
+}
