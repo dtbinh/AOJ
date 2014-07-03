9f8d8cf7133403bbfdf83eabf7893b35f756b449
okaoka <mnfrf593@yahoo.co.jp>
2014-07-03 09:55:28 +0900

    solve 2283_Seishun_18_Kippu

--- Volume_22/2283_Seishun_18_Kippu.cpp
+++ Volume_22/2283_Seishun_18_Kippu.cpp
@@ -40,16 +40,24 @@ int str2num(const string& station,map<string,int>& dict){
   return dict[station];
 }
 
+int dp[1001][1001];
+
 int main(){
   int total_stations;
   int total_lines;
+
   while(~scanf("%d %d",
 	       &total_stations,
 	       &total_lines)){
-    map<string,int> dist;
+    if(total_lines == 0) break;
+
+    map<string,int> dict;
     string shirokuro_location;
     string R2d_location;
     string Aizu_location;
+
+    cin >> shirokuro_location >> R2d_location >> Aizu_location;
+    memset(dp,0x3f,sizeof(dp));
 
     for(int line_idx = 0; line_idx < total_lines; line_idx++){
       string from,to;
@@ -57,6 +65,19 @@ int main(){
       int delay;
       cin >> from >> to >> distance >> delay;
       int cost = distance / 40 + delay;
+      dp[str2num(from,dict)][str2num(to,dict)] = cost;
+      dp[str2num(to,dict)][str2num(from,dict)] = cost;
     }
+
+    for(int k=0;k<total_stations;k++){
+      for(int i=0;i<total_stations;i++){
+	for(int j=0;j<total_stations;j++){
+	  dp[i][j] = min(dp[i][j],dp[i][k] + dp[k][j]);
+	}
+      }
+    }
+
+    printf("%d\n",dp[str2num(shirokuro_location,dict)][str2num(R2d_location,dict)]
+	   + dp[str2num(R2d_location,dict)][str2num(Aizu_location,dict)]);
   }
 }
