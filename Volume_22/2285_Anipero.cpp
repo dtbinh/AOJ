6de94d5ff72a5ee2c703bd02fd9a346d652d1695
okaoka <mnfrf593@yahoo.co.jp>
2014-07-04 21:38:38 +0900

    solve 2285_Anipero

--- Volume_22/2285_Anipero.cpp
+++ Volume_22/2285_Anipero.cpp
@@ -31,7 +31,7 @@ static const int ty[] = {-1,0,1,0};
 static const double EPS = 1e-8;
 
 // dp[picked secret][picked standard][money] ::= max satisfaction
-int dp[5][105][1005]; 
+int dp[3][105][1005]; 
 
 int main(){
   int money_limit;
@@ -45,10 +45,13 @@ int main(){
 	       &to_pick_standard_artists)){
     
     if(money_limit == 0
-       && total_secret_artists == 0) break;
+       && total_secret_artists == 0
+       && total_standard_artists == 0
+       && to_pick_standard_artists == 0) break;
 
-    memset(dp,0,sizeof(dp));
-    for(int i=0;i<total_secret_artists;i++){
+    memset(dp,-1,sizeof(dp));
+    dp[0][0][0] = 0;
+    for(int artist_idx=0;artist_idx<total_secret_artists;artist_idx++){
       string name;
       int cost;
       int satisfaction;
@@ -58,32 +61,36 @@ int main(){
 	for(int prev_money=1000;prev_money>=0;prev_money--){
 	  if(prev_money + cost > 1000) continue;
 
+	  if(dp[prev_picked][0][prev_money] == -1) continue;
 	  dp[prev_picked + 1][0][prev_money + cost]
 	    = max(dp[prev_picked][0][prev_money] + satisfaction,
 		  dp[prev_picked + 1][0][prev_money + cost]);
 	}
       }
     }
-    for(int i=0;i<total_standard_artists;i++){
+    for(int artist_idx=0;artist_idx<total_standard_artists;artist_idx++){
       string name;
       int cost;
       int satisfaction;
       cin >> name >> cost >> satisfaction;
-      for(int picked_secret=2;picked_secret>=0;picked_secret--){
-	for(int prev_picked_standard = 100; prev_picked_standard  >= 0; prev_picked_standard--){
+      for(int picked_secret=min(2,total_secret_artists);picked_secret>=1;picked_secret--){
+	for(int prev_picked_standard = artist_idx; prev_picked_standard  >= 0; prev_picked_standard--){
 	  for(int prev_money=1000;prev_money>=0;prev_money--){
 	  if(prev_money + cost > 1000) continue;
-	    dp[picked_secret][prev_picked_standard + 1][prev_money + cost]
-	      = max(dp[picked_secret][prev_picked_standard + 1][prev_money + cost],
-		    dp[picked_secret][prev_picked_standard][prev_money] + satisfaction);
+
+	  if(dp[picked_secret][prev_picked_standard][prev_money] == -1) continue;
+	  dp[picked_secret][prev_picked_standard + 1][prev_money + cost]
+	    = max(dp[picked_secret][prev_picked_standard + 1][prev_money + cost],
+		  dp[picked_secret][prev_picked_standard][prev_money] + satisfaction);
 	  }
+
 	}
       }	
     }
 
     int res = 0;
-    for(int picked_secret=2;picked_secret>=0;picked_secret--){
-      for(int picked_standard = 100; picked_standard  >= 0; picked_standard--){
+    for(int picked_secret=min(2,total_secret_artists);picked_secret>=1;picked_secret--){
+      for(int picked_standard = total_standard_artists; picked_standard  >=to_pick_standard_artists; picked_standard--){
 	for(int money=money_limit;money>=0;money--){
 	  res = max(dp[picked_secret][picked_standard][money],res);
 	}
