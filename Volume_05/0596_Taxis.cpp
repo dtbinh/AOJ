58a8f7b15dc4e4e3ff410505a96e00f8f67dd131
okaoka <mnfrf593@yahoo.co.jp>
2014-07-13 14:10:19 +0900

    WA

---
+++ Volume_05/0596_Taxis.cpp
@@ -0,0 +1,119 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+  
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+
+bool dp[5001][5001]; //dp[city][life] ::= bool
+
+class State{
+public:
+  int city;
+  int cost;
+  int life;
+  State(int _ci,int _co,int _l) :
+    city(_ci),cost(_co),life(_l) {}
+  bool operator<(const State& s) const {
+    cost < s.cost;
+  }
+  bool operator>(const State& s) const {
+    cost > s.cost;
+  }
+};
+
+struct Taxi{
+  int cost;
+  int life;
+  Taxi() : cost(0),life(0) {}
+  Taxi(int _c,int _l) : cost(_c), life(_l) {}
+};
+
+int main(){
+  int total_cities;
+  int total_roads;
+  while(~scanf("%d %d",&total_cities,&total_roads)){
+    Taxi taxi[5001];
+    memset(dp,false,sizeof(dp));
+    for(int city_idx = 0; city_idx < total_cities; city_idx++){
+      int cost;
+      int life;
+      scanf("%d %d",&cost,&life);
+      taxi[city_idx] = Taxi(cost,life);
+    }
+
+    vector<int> roads[5001];
+    for(int road_idx = 0;road_idx < total_roads;road_idx++){
+      int from,to;
+      scanf("%d %d",&from,&to);
+      from--; to--;
+      roads[from].push_back(to);
+      roads[to].push_back(from);
+    }
+    
+    priority_queue<State,vector<State>,greater<State> > que;
+    que.push(State(0,0,0));
+
+    int res = INF;
+    while(!que.empty()){
+      State s = que.top();
+      que.pop();
+
+      if(dp[s.city][s.life]) continue;
+      dp[s.city][s.life] = true;
+      if(s.city == total_cities - 1){
+	res = min(res,s.cost);
+      }
+      for(int i=0; i < roads[s.city].size(); i++){
+
+	int next_city = roads[s.city][i];
+	int next_life = s.life - 1;
+	if(next_life < 0){
+	  //have to ride
+	  int next_cost = s.cost + taxi[s.city].cost;
+	  next_life = taxi[s.city].life - 1;
+	  que.push(State(next_city,next_cost,next_life));
+	}
+	else{
+	  //ride
+	  int next_cost = s.cost + taxi[s.city].cost;
+	  next_life = taxi[s.city].life - 1;
+	  que.push(State(next_city,next_cost,next_life));
+
+	  //don't ride
+	  next_cost = s.cost;
+	  next_life = s.life - 1;
+	  que.push(State(next_city,next_cost,next_life));
+	}
+      }
+    }
+
+    printf("%d\n",res);
+  }
+}
