14e4dae8363302df16d4ad201ba3dc456fc404c5
okaoka <mnfrf593@yahoo.co.jp>
2014-07-16 00:17:50 +0900

    solve 0580_Fish

--- Volume_05/0580_Fish.cpp
+++ Volume_05/0580_Fish.cpp
@@ -32,92 +32,123 @@ static const int ty[] = {-1,0,1,0};
 
 class Cuboid{
 public:
-  int x[8];
-  int y[8];
-  int d[8];
+  ll ox[8]; //original x
+  ll oy[8];
+  ll od[8];
+
+  int mx[8]; // mapped x
+  int my[8];
+  int md[8];
+
   Cuboid(int x1,int y1,int d1,
 	 int x2,int y2,int d2){
-    x[0] = x1;
-    y[0] = y1;
-    d[0] = d1;
+    ox[0] = x1;
+    oy[0] = y1;
+    od[0] = d1;
 
-    x[1] = x1;
-    y[1] = y2;
-    d[1] = d1;
+    ox[1] = x1;
+    oy[1] = y2;
+    od[1] = d1;
 
-    x[2] = x2;
-    y[2] = y2;
-    d[2] = d1;
+    ox[2] = x2;
+    oy[2] = y2;
+    od[2] = d1;
 
-    x[3] = x2;
-    y[3] = y1;
-    d[3] = d1;
+    ox[3] = x2;
+    oy[3] = y1;
+    od[3] = d1;
 
-    x[4] = x1;
-    y[4] = y1;
-    d[4] = d2;
+    ox[4] = x1;
+    oy[4] = y1;
+    od[4] = d2;
 
-    x[5] = x1;
-    y[5] = y2;
-    d[5] = d2;
+    ox[5] = x1;
+    oy[5] = y2;
+    od[5] = d2;
 
-    x[6] = x2;
-    y[6] = y2;
-    d[6] = d2;
+    ox[6] = x2;
+    oy[6] = y2;
+    od[6] = d2;
 
-    x[7] = x2;
-    y[7] = y1;
-    d[7] = d2;
+    ox[7] = x2;
+    oy[7] = y1;
+    od[7] = d2;
   }    
 
-  int compute_volume(int _x[8],int _y[8],int _d[8]){
-    int top_area = (_x[1] - _x[2]) * (_y[1] - _y[0]);
-    int height = _d[4] - _d[0];
-    return (top_area * height < 0 ? 0 : top_area * height);
+  void mapping(const vector<ll>& xs,
+	       const vector<ll>& ys,
+	       const vector<ll>& ds){
+    for(int i=0;i<8;i++){
+      mx[i] = lower_bound(xs.begin(),xs.end(),ox[i]) - xs.begin();
+      my[i] = lower_bound(ys.begin(),ys.end(),oy[i]) - ys.begin();
+      md[i] = lower_bound(ds.begin(),ds.end(),od[i]) - ds.begin();
+    }
   }
-
-  int operator&(const Cuboid& c) const{
-    int next_x[8];
-    int next_y[8];
-    int next_d[8];
-
-    next_x[0] = max(x[0],c.x[0]);
-    next_y[0] = max(y[0],c.y[0]);
-    next_d[0] = max(d[0],c.d[0]);
-
-    next_x[1] = max(x[1],c.x[1]);
-    next_y[1] = min(y[1],c.y[1]);
-    next_d[1] = max(d[1],c.d[1]);
-
-    next_x[2] = min(x[2],c.x[2]);
-    next_y[2] = min(y[2],c.y[2]);
-    next_d[2] = max(d[2],c.d[2]);
-
-    next_x[3] = min(x[3],c.x[3]);
-    next_y[3] = max(y[3],c.y[3]);
-    next_d[3] = max(d[3],c.d[3]);
-
-    next_x[4] = max(x[4],c.x[4]);
-    next_y[4] = max(y[4],c.y[4]);
-    next_d[4] = min(d[4],c.d[4]);
-
-    next_x[5] = max(x[5],c.x[5]);
-    next_y[5] = min(y[5],c.y[5]);
-    next_d[5] = min(d[5],c.d[5]);
-
-    next_x[6] = min(x[6],c.x[6]);
-    next_y[6] = min(y[6],c.y[6]);
-    next_d[6] = min(d[6],c.d[6]);
-
-    next_x[7] = min(x[7],c.x[7]);
-    next_y[7] = max(y[7],c.y[7]);
-    next_d[7] = min(d[7],c.d[7]);
-
-    return 0;
-  }
-
 };
 
+int dp[101][101][101];
+
 int main(){
-  
+  int total_fish;
+  int lower;
+  while(~scanf("%d %d",&total_fish,&lower)){
+
+    vector<ll> xs;
+    vector<ll> ys;
+    vector<ll> ds;
+
+    vector<Cuboid> cuboids;
+    for(int fish_idx = 0; fish_idx < total_fish; fish_idx++){
+      ll x1,y1,d1;
+      ll x2,y2,d2;
+      scanf("%lld %lld %lld",&x1,&y1,&d1);
+      scanf("%lld %lld %lld",&x2,&y2,&d2);
+      
+      cuboids.push_back(Cuboid(x1,y1,d1,x2,y2,d2));
+      xs.push_back(x1);       
+      xs.push_back(x2);
+
+      ys.push_back(y1);       
+      ys.push_back(y2);
+
+      ds.push_back(d1);       
+      ds.push_back(d2);
+    }
+
+    sort(xs.begin(),xs.end());
+    sort(ys.begin(),ys.end());
+    sort(ds.begin(),ds.end());
+
+    xs.erase(unique(xs.begin(),xs.end()),xs.end());
+    ys.erase(unique(ys.begin(),ys.end()),ys.end());
+    ds.erase(unique(ds.begin(),ds.end()),ds.end());
+
+    memset(dp,0,sizeof(dp));
+    for(int i=0;i<cuboids.size();i++){
+      cuboids[i].mapping(xs,ys,ds);
+
+      for(int x = cuboids[i].mx[0]; x < cuboids[i].mx[2]; x++){
+	for(int y = cuboids[i].my[0]; y < cuboids[i].my[1]; y++){
+	  for(int d = cuboids[i].md[0]; d < cuboids[i].md[4]; d++){
+	    dp[x][y][d]++;
+	  }
+	}
+      }
+    }
+
+    ll res = 0;
+    for(int x = 0; x < xs.size(); x++){
+      for(int y = 0; y < ys.size(); y++){
+	for(int d = 0; d < ds.size(); d++){
+	  if(dp[x][y][d] >= lower){
+	    res += (xs[x + 1] - xs[x])
+	      * (ys[y + 1] - ys[y])
+	      * (ds[d + 1] - ds[d]);
+	  }
+	}
+      }
+    }
+
+    printf("%lld\n",res);
+  }
 }
