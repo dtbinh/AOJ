a8e787af4cd48158cbe8a9ad63fb1e4f673ea913
okaoka <mnfrf593@yahoo.co.jp>
2014-07-21 00:28:24 +0900

    solve 2536_Median_Tree

--- Volume_25/2536_Median_Tree.cpp
+++ Volume_25/2536_Median_Tree.cpp
@@ -49,6 +49,11 @@ public:
     }
   }
 
+  ~UnionFindTree(){
+    delete[] mParent;
+    delete[] mRank;
+  }
+
   bool same(int lhs,int rhs){
     return (find(lhs) == find(rhs) ? true : false);
   }
@@ -61,6 +66,8 @@ public:
   void unite(int lhs,int rhs){
     lhs = find(lhs);
     rhs = find(rhs);
+    
+    if(lhs == rhs) return;
     
     if(mRank[lhs] > mRank[rhs]){
       mParent[lhs] = rhs;
@@ -79,16 +86,48 @@ public:
   }
 };
 
+class State{
+public:
+  int from;
+  int to;
+  int cost;
+  State(int _f,int _t,int _c) :
+    from(_f), to(_t), cost(_c) {}
+  bool operator<(const State& s) const{
+    return cost < s.cost;
+  }
+  bool operator>(const State& s) const{
+    return cost > s.cost;
+  }
+};
+
 int main(){
   int num_of_nodes;
   int num_of_edges;
-  while(~scanf("%d",&num_of_nodes,&num_of_edges)){
+  while(~scanf("%d %d",&num_of_nodes,&num_of_edges)){
     if(num_of_nodes == 0 && num_of_edges == 0) break;
 
+    priority_queue<State,vector<State>,greater<State> > que;
     for(int edge_idx = 0; edge_idx < num_of_edges; edge_idx++){
       int from,to,cost;
       scanf("%d %d %d",&from,&to,&cost);
+      from--; to--;
+      que.push(State(from,to,cost));
     }
+
+    UnionFindTree uft(num_of_nodes);
+
+    vector<int> log;
+    while(!que.empty()){
+      State s = que.top();
+      que.pop();
+      if(!uft.same(s.from,s.to)){
+	uft.unite(s.from,s.to);
+	log.push_back(s.cost);
+      }
+    }
+
+    printf("%d\n",log[log.size()/2]);
   }
 }
 
