db452691ee7cacf7df82afdc2b611a4d81baa893
okaoka <mnfrf593@yahoo.co.jp>
2014-08-17 19:14:51 +0900

    wrote input portion

---
+++ Volume_25/2582_Step_Aerobics.cpp
@@ -0,0 +1,42 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+  
+int tx[] = {0,1,0,-1};
+int ty[] = {-1,0,1,0};
+
+int main(){
+  int total_steps;
+  while(~scanf("%d",&total_steps)){
+    for(int step_idx = 0; step_idx < total_steps; step_idx++){
+      char command[12];
+      scanf("%s",command);
+    }
+  }
+}
