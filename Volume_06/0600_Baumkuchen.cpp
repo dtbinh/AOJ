0bf60ef7bbdbae0f280ae7d3799a8d27cd502ce8
okaoka <mnfrf593@yahoo.co.jp>
2014-08-28 23:45:18 +0900

    solve 0600_Baumkuchen

--- Volume_06/0600_Baumkuchen.cpp
+++ Volume_06/0600_Baumkuchen.cpp
@@ -30,19 +30,49 @@ static const double EPS = 1e-8;
 static const int tx[] = {0,1,0,-1};
 static const int ty[] = {-1,0,1,0};
 
+ll sum[200001];
+
 int main(){
   int N;
   while(~scanf("%d",&N)){
-    vector<int> baumkuchen;
+    vector<ll> baumkuchen;
 
-    int sum[100001];
     memset(sum,0,sizeof(sum));
     for(int i=0;i<N;i++){
-      int width;
-      scanf("%d",width);
+      ll width;
+      scanf("%lld",&width);
       baumkuchen.push_back(width);
-      sum[i] = (i - 1 >=0 ? sum[i - 1] : 0) + width;
+      sum[i+1] = sum[i] + width;
     }
     
+    for(int i=0;i<N;i++){
+      sum[N + i + 1] = sum[N] + sum[i + 1];
+    }
+
+    ll lhs = 0;
+    ll rhs = 100000000000000LL;
+    for(int round = 0; round < 50; round++){
+      ll mid = lhs + (rhs - lhs) / 2LL;
+
+      bool isok = false;
+      for(int i=0;i<N;i++){
+	ll offset = sum[i];
+	int div_idx = lower_bound(sum,sum + 2 * N,offset + mid) - sum;
+	div_idx = lower_bound(sum,sum + 2 * N,sum[div_idx] + mid) - sum;
+	div_idx = lower_bound(sum,sum + 2 * N,sum[div_idx] + mid) - sum;
+	if(div_idx <= i + N){
+	  isok = true;
+	  break;
+	}
+      }
+      if(isok){
+	lhs = mid;
+      }
+      else{
+	rhs = mid;
+      }
+    }
+
+    printf("%lld\n",lhs);
   }
 }
