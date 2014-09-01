3dae438a64a1e53cf13ca6da44f4486440a4c0bf
okaoka <mnfrf593@yahoo.co.jp>
2014-09-02 01:12:16 +0900

    answer is correct

--- Volume_23/2304_Reverse_Roads.cpp
+++ Volume_23/2304_Reverse_Roads.cpp
@@ -31,48 +31,55 @@ const static int ty[] = {-1,-1,0,1,1,1,0,-1};
  
 static const double EPS = 1e-8;
 
+int path[301][301];
+
 class Edge{
 public:
   int _to;
   int _capacity;
   int _reverse;
+  int _id;
   Edge(int to,int capacity,int reverse) :
     _to(to), _capacity(capacity), _reverse(reverse) {}
+  Edge(int to,int capacity,int reverse,int id) :
+    _to(to), _capacity(capacity), _reverse(reverse),_id(id) {}
 };
 
 class FordFulkerson {
 private:
   int _n;
-  bool* _used;
+  bool *_used;
   vector<Edge>* _edges;
-  map<P,int> _id;
+  void fill_used(){
+    fill(_used,_used + (_n+1) ,false);
+  }
 public:
   FordFulkerson(int n){
     _n = n;
-    _used = new bool[n+1]();
     _edges = new vector<Edge>[n+1];
+    _used = new bool[n+1]();
   }
-  void set_id(map<P,int> id){
-    _id = id;
-  }
-  
-  void add_edge(int from,int to,int capacity = 1){
-    _edges[from].push_back(Edge(to,capacity,_edges[to].size()));
-    _edges[to].push_back(Edge(from,capacity,_edges[from].size()-1));
+
+  void add_edge(int from,int to,int capacity,int id){
+    _edges[from].push_back(Edge(to,capacity,_edges[to].size(),id));
+    _edges[to].push_back(Edge(from,capacity,_edges[from].size()-1,-id));
   }
 
   int dfs(int current,int sink,int flow){
     if(current == sink) return flow;
     _used[current] = true;
     for(int i=0;i<_edges[current].size();i++){
-      int to = _edges[current][i]._to;
-      if(_used[to]) continue;
+      Edge& e = _edges[current][i];
+      int to = e._to;
+      if(_used[to] || e._capacity <= 0) continue;
 
-      int d = dfs(to,sink,min(flow,_edges[current][i]._capacity));
+      int d = dfs(to,sink,min(flow,e._capacity));
       if(d <= 0) continue;
       
-      _edges[current][i]._capacity -= d;
-      _edges[to][_edges[current][i]._reverse]._capacity += d;
+      e._capacity -= d;
+      _edges[to][e._reverse]._capacity += d;
+      path[current][to] -= d;
+      path[to][current] += d;
       return d;
     }
     
@@ -82,6 +89,7 @@ public:
   int compute_max_flow(int source,int sink){
     int res = 0;
     while(true){
+      fill_used();
       int tmp = dfs(source,sink,INF);
       if(tmp == 0) break;
       res += tmp;
@@ -89,17 +97,14 @@ public:
     return res;
   }
 
-  set<int> compute_reverse(){
+  set<int> compute_reverse(const vector<P>& roads,int path[301][301]){
     set<int> res;
-    for(map<P,int>::iterator it = _id.begin(); it != _id.end(); it++){
-      int from = it->first.first;
-      int to = it->first.second;
-      for(int i = 0; i < _edges[from].size();i++){
-      	if(to == _edges[from][i]._to){
-	  if(_edges[from][i]._capacity > 0){
-	    res.insert(it->second);
-	  }
-      	}
+    for(int road_i = 0; road_i < roads.size();road_i++){
+      int from = roads[road_i].first;
+      int to = roads[road_i].second;
+
+      if(path[to][from] == 0){
+	 res.insert(road_i + 1);
       }
     }
     return res;
@@ -114,20 +119,22 @@ int main(){
 	       &num_of_roads)){
     
     FordFulkerson fordfulkerson(num_of_crossings);
-    map<P,int> id;
-    for(int road_i = 0; road_i < num_of_roads; road_i++){
+
+    memset(path,0,sizeof(path));
+    vector<P> roads;
+    for(int road_i = 1; road_i <= num_of_roads; road_i++){
       int from,to;
       scanf("%d %d",&from,&to);
-      fordfulkerson.add_edge(from,to);
-      // fordfulkerson.add_edge(to,from);
-      id[P(from,to)] = id.size();
+      fordfulkerson.add_edge(from,to,1,road_i);
+      path[from][to] = path[to][from] = 1;
+      roads.push_back(P(from,to));
     }
+
     int source,sink;
     scanf("%d %d",&source,&sink);
     cout << fordfulkerson.compute_max_flow(source,sink) << endl;
     
-    fordfulkerson.set_id(id);
-    set<int> res = fordfulkerson.compute_reverse();
+    set<int> res = fordfulkerson.compute_reverse(roads,path);
     cout << res.size() << endl;
     for(set<int>::iterator it = res.begin(); it != res.end(); it++){
       cout << *it << endl;
