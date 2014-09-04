48d6f642cfcc059506dad166e3eccf0ce43c4b78
okaoka <mnfrf593@yahoo.co.jp>
2014-09-05 01:14:56 +0900

    solve 2306_Rabbit_Party

--- Volume_23/2306_Rabbit_Party.cpp
+++ Volume_23/2306_Rabbit_Party.cpp
@@ -33,11 +33,10 @@ static const double EPS = 1e-8;
 
 class RelationShip{
 public:
-  int _from;
   int _to;
   int _friendliness;
-  RelationShip(int from,int to,int friendliness) :
-    _from(from), _to(to), _friendliness(friendliness) {}
+  RelationShip(int to,int friendliness) :
+    _to(to), _friendliness(friendliness) {}
   bool operator<(const RelationShip &r) const {
     return _friendliness < r._friendliness;
   }
@@ -46,84 +45,66 @@ public:
   }
 };
 
-class UnionFindTree {
-private:
-  int* par;
-  int* rank;
-  int* weight;
-public:
-  UnionFindTree(int n){
-    par = new int[n]();
-    rank = new int[n]();
-    weight = new int[n]();
-    for(int i=0;i<n;i++){
-      par[i] = i;
-      rank[i] = 0;
-      weight[i] = 1;
+
+bool used[101];
+vector<RelationShip> edges[101];
+int idx[101][101];
+int total_rabbits;
+int total_pairs;
+
+int dfs(int last){
+  int res = 0;
+
+  int sum = 0;
+  for(int orig=0; orig <= last; orig++){
+    if(!used[orig]) continue;
+
+    int tmp = INF;
+    for(int next_i = 0; next_i < edges[orig].size(); next_i++){
+      int next = edges[orig][next_i]._to;
+      if(!used[next]) continue;
+      tmp = min(tmp,edges[orig][next_i]._friendliness);
     }
+    if(tmp != INF) sum += tmp;
   }
+  
+  res = max(sum,res);
 
-  ~UnionFindTree(){
-    delete[] rank;
-    delete[] par;
-    delete[] weight;
-  }
-
-  int find(int x){
-    if(par[x] == x){
-      return x;
-    } else {
-      return par[x] = find(par[x]);
+  for(int next=last + 1; next < total_rabbits; next++){
+    bool isok = true;
+    for(int prev = 0; prev <= last; prev++){
+      if(used[prev] && idx[prev][next] == -1) {
+	isok = false;
+	break;
+      }
     }
-  }
 
-  void unite(int x,int y){
-    x = find(x);
-    y = find(y);
-    if (x == y) return;
-
-    if(rank[x] < rank[y]){
-      par[x] = y;
-      weight[y] += weight[x];
-    } else {
-      par[y] = x;
-      weight[x] += weight[y];
-      if (rank[x] == rank[y]) rank[x]++;
-    }
+    if(!isok) continue;
+    used[next] = true;
+    res = max(res,dfs(next));
+    used[next] = false;
   }
-
-  int count(int x){
-    return weight[find(x)];
-  }
-  bool same(int x,int y){
-    return find(x) == find(y);
-  }
-};
+  return res;
+}
 
 int main(){
-  int total_rabbits;
-  int total_pairs;
   while(~scanf("%d %d",&total_rabbits,&total_pairs)){
-    UnionFindTree uft(total_rabbits);
+    for(int i=0;i<total_rabbits;i++){
+      edges[i].clear();
+    }
+    memset(used,false,sizeof(used));   
+    memset(idx,-1,sizeof(idx));
 
-    priority_queue<RelationShip> que;
     for(int pair_i = 0; pair_i < total_pairs; pair_i++){
       int from,to,friendliness;
       scanf("%d %d %d",&from,&to,&friendliness);
       from--; to--;
-      que.push(RelationShip(from,to,friendliness));
+      edges[from].push_back(RelationShip(to,friendliness));
+      edges[to].push_back(RelationShip(from,friendliness));
+      idx[from][to] = edges[from].size() - 1;
+      idx[to][from] = edges[to].size() - 1;
     }
 
-
-    int res = 0;
-    while(!que.empty()){
-      RelationShip rs = que.top();
-      que.pop();
-      if(uft.same(rs._from,rs._to)) continue;
-      uft.unite(rs._from,rs._to);
-      res += rs._friendliness;
-    }
-
-    printf("%d\n",res);
+    cout << dfs(-1) << endl;
   }
 }
