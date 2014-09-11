2a340fe51e5f268943a51c3e29c42859063e79cd
okaoka <mnfrf593@yahoo.co.jp>
2014-09-11 23:47:23 +0900

    solve 2326_Number_Sorting

--- Volume_23/2326_Number_Sorting.cpp
+++ Volume_23/2326_Number_Sorting.cpp
@@ -31,58 +31,63 @@ const static int ty[] = {-1,+0,+1,+0};
  
 static const double EPS = 1e-8;
 
-ll patterns[100001];
+class BIT {
+private:
+  int n;
+  ll* bit;
+public:
+  BIT(int _n) {
+    n = _n;
+    bit = new ll[n]();
+  }
+  
+  ll sum(int i){
+    ll s = 0;
+    while(i > 0){
+      s += bit[i];
+      i -= i & -i;
+    }
+    return s;
+  }
+  
+  void add(int i, ll x){
+    while(i <= n){
+      bit[i] += x;
+      i += i & -i;
+    }
+  }
+};
 
 int main(){
-  ll first_num;
-  ll last_num;
-  ll mod;
-
-  patterns[0] = 0;
-  patterns[1] = 1;
-  for(int range = 1; range < 100000; range++){
-    patterns[range+1] += (2LL * patterns[range]) % 1000000007LL;
-  }
-
-  while(~scanf("%lld %lld %lld",&first_num,&last_num,&mod)){
+  int first_num;
+  int last_num;
+  int mod;
+  while(~scanf("%d %d %d",&first_num,&last_num,&mod)){
     if(first_num == 0 && last_num == 0 && mod == 0) break;
-
-
-    map<ll,ll> dp;
-    map<ll,ll> order_lexicon;
+    
+    vector<int> order_lexicon;
     vector<string> nums;
+    
     for(int start = first_num; start <= last_num; start++){
       stringstream ss;
       ss << start;
       nums.push_back(ss.str());
     }
     sort(nums.begin(),nums.end());
-
+    
     for(int num_i = 0; num_i < nums.size(); num_i++){
-      order_lexicon[atol(nums[num_i].c_str())] = num_i;
+      order_lexicon.push_back(atoi(nums[num_i].c_str()));
     }
-
+    
     ll res = 0;
-    for(ll start = first_num; start <= last_num; start++){
-      ll lhs = start;
-      ll rhs = last_num;
-      
-      for(int round = 0; round < 20; round++){
-	ll mid = lhs + (rhs - lhs) / 2;
-	ll range = order_lexicon[mid] - order_lexicon[start] + 1;
-	if(range == mid - start + 1){
-	  lhs = mid;
-	}
-	else {
-	  rhs = mid;
-	}
-      }
-      
-      printf("start:%lld range:%lld patterns:%lld\n",start,lhs - start + 1,patterns[lhs - start + 1 - 1] + 1);
-      res += patterns[lhs-start+1 - 1] + 1;
+    BIT bit(last_num - first_num + 2);
+    for(int i = 0; i < order_lexicon.size();i++){
+      ll tmp = (bit.sum(order_lexicon[i] - first_num + 1) + 1) % mod;
+      res += tmp;
       res %= mod;
+      bit.add(order_lexicon[i] - first_num + 1,tmp);
     }
-
-    printf("%lld\n",res);
+    
+    printf("%lld\n",res % mod);
   }
 }
