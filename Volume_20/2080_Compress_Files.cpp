7b9af3a730755cd348befe3049deed9aff54f4ed
okaoka <mnfrf593@yahoo.co.jp>
2014-09-16 18:52:17 +0900

    solved 2080_Compress_Files

---
+++ Volume_20/2080_Compress_Files.cpp
@@ -0,0 +1,106 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {0,1,0,-1};
+int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+class State {
+public:
+  int _used;
+  int _archives;
+  State(int used,int archives) :
+    _used(used), _archives(archives) {}
+  bool operator <(const State& s) const {
+    return _archives < s._archives;
+  }
+  bool operator >(const State& s) const {
+    return _archives > s._archives;
+  }
+};
+
+int open_space[1<<14];
+int required_space[1<<14];
+int dp[1<<14];
+
+int main(){
+  int total_files;
+  int init_disk_space;
+  while(~scanf("%d %d",&total_files,&init_disk_space)){
+    if(total_files == 0 && init_disk_space == 0) break;
+
+    vector<int> diff;
+    vector<int> req;
+    for(int file_i = 0; file_i < total_files; file_i++){
+      int uncompressed,compressed;
+      scanf("%d %d",&uncompressed,&compressed);
+      diff.push_back(uncompressed - compressed);
+      req.push_back(compressed);
+    }
+    
+    memset(open_space,0,sizeof(open_space));
+    memset(required_space,0,sizeof(required_space));
+    memset(dp,0x3f,sizeof(dp));
+
+    for(int S = 0; S <= (1<<total_files) - 1; S++){
+      int open_sum = 0;
+      int req_sum = 0;
+      for(int file_i = 0; file_i < total_files; file_i++){
+	if(S & (1 << file_i)){
+	  open_sum += diff[file_i];
+	  req_sum += req[file_i];
+	}
+      }
+      open_space[S] = open_sum;
+      required_space[S] = req_sum;
+    }
+
+    priority_queue<State,vector<State>,greater<State> > que;
+    que.push(State(0,0));
+    dp[0] = 0;
+    while(!que.empty()){
+      State s = que.top();
+      que.pop();
+      
+      for(int S = 0; S <= (1<<total_files) - 1; S++){
+	if(s._used & S) continue;
+	if(dp[s._used | S] <= s._archives + 1) continue;
+	if(required_space[S] > open_space[s._used] + init_disk_space) continue;
+	dp[s._used | S] = s._archives + 1;
+	que.push(State(s._used | S,s._archives + 1));
+      }
+    }
+
+    if(dp[(1<<total_files) - 1] >= INF){
+      printf("Impossible\n");
+    }
+    else{
+      printf("%d\n",dp[(1<<total_files) - 1]);
+    }
+  }
+}
