697bd28f65ea22e843b2e7b1c1dda0c64fb04229
okaoka <mnfrf593@yahoo.co.jp>
2014-09-18 00:11:30 +0900

    add some geometrical functions

--- Volume_20/2081_Save_the_Energy.cpp
+++ Volume_20/2081_Save_the_Energy.cpp
@@ -37,16 +37,71 @@ public:
   int _z;
   Point(int x,int y,int z) :
     _x(x), _y(y), _z(z) {}
-};
-
-class Line {
-public:
-  vector<Point> l;
-  Line(const Point& p1,const Point& p2) {
-    l.push_back(p1);
-    l.push_back(p2);
+  Point operator-(const Point& p) const {
+    return Point(_x - p._x,
+		 _y - p._y,
+		 _z - p._z);
+  }
+  Point operator+(const Point& p) const {
+    return Point(_x + p._x,
+		 _y + p._y,
+		 _z + p._z);
+  }
+  Point operator*(const double t) const {
+    return Point(_x * t,
+		 _y * t,
+		 _z * t);
   }
 };
+
+class Line : public vector<Point> {
+public:
+  Line(const Point& p1,const Point& p2) {
+    push_back(p1);
+    push_back(p2);
+  }
+};
+
+
+double norm(const Point& p){
+  return sqrt(p._x * p._x + p._y * p._y + p._z * p._z);
+}
+
+double dot(const Point& p1,const Point& p2){
+  return p1._x * p2._x + p1._y * p2._y + p1._z * p2._z;
+}
+
+double abs(const Point& p){
+  return sqrt(dot(p,p));
+}
+
+Point cross(const Point& p1,const Point& p2){
+  return Point(p1._y * p2._z - p1._z * p2._y,
+	       p1._z * p2._x - p1._x * p2._z,
+	       p1._x * p2._y - p1._y * p2._x);
+}
+
+Point projection(const Line& l,const Point& p){
+  double t = dot(p - l[0],l[0]-l[1]) / norm(l[0] - l[1]);
+  return l[0] + (l[0] - l[1]) * t;
+}
+
+Point reflection(const Line& l,const Point& p){
+  return p + (projection(l,p) - p) * 2;
+}
+
+double distanceLP(const Line& l,const Point& p){
+  return abs(p - projection(l,p));
+}
+
+bool intersectLL(const Line &l, const Line &m) {
+  return abs(cross(l[1]-l[0], m[1]-m[0])) > EPS || // non-parallel
+    abs(cross(l[1]-l[0], m[0]-l[0])) < EPS;   // same line
+}
+
+double distanceLL(const Line& l,const Line& m){
+  return intersectLL(l,m) ? 0 : distanceLP(l,m[0]);
+}
 
 int main(){
   int num_of_straight_paths;
@@ -55,7 +110,7 @@ int main(){
     int gx,gy,gz;
     scanf("%d %d %d",&sx,&sy,&sz);
     scanf("%d %d %d",&gx,&gy,&gz);
-
+    
     vector<Line> lines;
     for(int path_i = 0; path_i < num_of_straight_paths; path_i++){
       int x[2],y[2],z[2];
@@ -65,5 +120,7 @@ int main(){
       lines.push_back(Line(Point(x[0],y[0],z[0]),
 			   Point(x[1],y[1],z[1])));
     }
+
+    
   }
 }
