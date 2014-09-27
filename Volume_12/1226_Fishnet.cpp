2792d3c556f84898c03e5dd7b1522530aacf3019
okaoka <mnfrf593@yahoo.co.jp>
2014-09-28 01:09:10 +0900

    solve 1226_Fishnet

--- Volume_12/1226_Fishnet.cpp
+++ Volume_12/1226_Fishnet.cpp
@@ -46,11 +46,6 @@ struct Line : public vector<Point> {
   }
 };
 
-// bool intersectSS(const Line &s, const Line &t) {
-//   return ccw(s[0],s[1],t[0]) * ccw(s[0],s[1],t[1]) <= 0 &&
-//     ccw(t[0],t[1],s[0]) * ccw(t[0],t[1],s[1]) <= 0;
-// }
-
 Point crosspoint(const Line &l, const Line &m) {
   double A = cross(l[1] - l[0], m[1] - m[0]);
   double B = cross(l[1] - l[0], l[1] - m[0]);
@@ -59,17 +54,33 @@ Point crosspoint(const Line &l, const Line &m) {
   return m[0] + B / A * (m[1] - m[0]);
 }
 
+double compute_area(const Point& a,const Point& b,
+		    const Point& c,const Point& d){
+  return 1.0/2.0 * ((a.real() - b.real())*(a.imag() + b.imag())
+		    + (b.real() - c.real())*(b.imag() + c.imag())
+		    + (c.real() - d.real())*(c.imag() + d.imag())
+		    + (d.real() - a.real())*(d.imag() + a.imag()));
+}
+
 int main(){
   const double frame_x[] = {0.0, 0.0, 0.0, 1.0};
   const double frame_y[] = {0.0, 1.0, 0.0, 0.0};
   int num_of_pegs;
 
   while(~scanf("%d",&num_of_pegs)){
+    if(num_of_pegs == 0) break;
+
     vector<Point> bottom;
     vector<Point> top;
     vector<Point> left;
     vector<Point> right;
-    
+
+    Point points[50][50];  
+    points[0][0] = Point(0,0);
+    points[0][num_of_pegs + 1] = Point(0,1);
+    points[num_of_pegs + 1][0] = Point(1,0);
+    points[num_of_pegs + 1][num_of_pegs + 1] = Point(1,1);
+
     for(int type = 0; type < 4; type++){
       for(int peg_i = 0; peg_i < num_of_pegs; peg_i++){
 	double pos;
@@ -80,24 +91,28 @@ int main(){
 	  x = frame_x[type] + pos;
 	  y = frame_y[type];
 	  bottom.push_back(Point(x,y));
+	  points[peg_i+1][0] = Point(x,y);
 	}
 	else if(type == 1){
 	  //top
 	  x = frame_x[type] + pos;
 	  y = frame_y[type];
 	  top.push_back(Point(x,y));
+	  points[peg_i+1][num_of_pegs + 1] = Point(x,y);
 	}
 	else if(type == 2){
 	  //left
 	  x = frame_x[type];
 	  y = frame_y[type] + pos;
 	  left.push_back(Point(x,y));
+	  points[0][peg_i + 1] = Point(x,y);
 	}
 	else{
 	  //right
 	  x = frame_x[type];
 	  y = frame_y[type] + pos;
 	  right.push_back(Point(x,y));
+	  points[num_of_pegs + 1][peg_i + 1] = Point(x,y);
 	}
       }
     }
@@ -114,9 +129,25 @@ int main(){
       horizontal.push_back(line);
     } 
 
-    for(int i = 0; i < top.size(); i++){
-      Point p = crosspoint(vertical[i],horizontal[i]);
-      cout << p.real() << " " << p.imag() << endl;
+    for(int i = 0; i < vertical.size(); i++){
+      for(int j = 0; j < horizontal.size(); j++){
+	Point p = crosspoint(vertical[i],horizontal[j]);
+	points[i+1][j+1] = p;
+      }
     }
+
+    double max_area = 0.0;
+    for(int x = 0; x+1 < num_of_pegs + 2; x++){
+      for(int y = 0; y+1 < num_of_pegs + 2; y++){
+	
+	Point left_bottom = points[x][y];
+	Point right_bottom = points[x+1][y];
+	Point left_top = points[x][y+1];
+	Point right_top = points[x+1][y+1];
+	max_area = max(max_area,
+		       compute_area(left_bottom,right_bottom,right_top,left_top));
+      }
+    }
+    printf("%.7lf\n",max_area);
   }
 }
