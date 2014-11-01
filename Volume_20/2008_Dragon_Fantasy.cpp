9dbbe84be982d6e63008778da374d3e6c07d0fda
okaoka <mnfrf593@yahoo.co.jp>
2014-11-01 15:07:09 +0900

    MLE

--- Volume_20/2008_Dragon_Fantasy.cpp
+++ Volume_20/2008_Dragon_Fantasy.cpp
@@ -28,7 +28,9 @@ typedef pair <int,P > PP;
 int tx[] = {0,1,0,-1};
 int ty[] = {-1,0,1,0};
  
-static const double EPS = 1e-8;
+static const double EPS = 1e-12;
+
+double dp[1<<21][21];
 
 int main(){
   int total_cristals;
@@ -44,9 +46,43 @@ int main(){
 	       &monster_y)){
     if(total_cristals == 0 && hero_x == 0 && hero_y == 0
        && monster_x == 0 && monster_y == 0) break;
+    vector<P> cristals;
+    cristals.push_back(P(hero_x,hero_y));
     for(int cristal_i = 0; cristal_i < total_cristals; cristal_i++){
       int x,y;
       scanf("%d %d",&x,&y);
+      cristals.push_back(P(x,y));
     }
+    fill((double*)dp,(double*)dp + (1<<21) * 21,1000000000000);
+    dp[(1<<0)][0] = 0;
+    for(int S = 0; S < (1<<cristals.size()); S++){
+      for(int from = 0; from < cristals.size(); from++){
+	if(!(S & (1<<from))) continue;
+	for(int to = 0; to < cristals.size(); to++){
+	  if(S & (1<<to)) continue;
+	  double diff_x = cristals[from].first - cristals[to].first;
+	  double diff_y = cristals[from].second - cristals[to].second;
+	  double dist = sqrt(diff_x * diff_x + diff_y * diff_y); 
+
+	  double impurity = 
+	    sqrt((monster_x - cristals[to].first) * (monster_x - cristals[to].first)
+		 + (monster_y - cristals[to].second) * (monster_y - cristals[to].second));
+	  if(impurity > dp[S][from] + dist){
+	    dp[S | (1<<to)][to] = min(dp[S][from] + dist,
+				      dp[S | (1<<to)][to]);
+	  }
+	}
+      }
+    }
+    
+    bool res = false;
+    for(int to = 0; to < cristals.size(); to++){
+      if(dp[(1<<cristals.size()) - 1][to] < 1000000000000){
+	res = true;
+	break;
+      }
+    }
+
+    printf("%s\n",res ? "YES" : "NO");
   }
 }
