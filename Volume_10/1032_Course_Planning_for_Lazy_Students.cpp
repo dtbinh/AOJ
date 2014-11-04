fa5a28a0f92a3f18c3ab5eacf3bd20185c2b7d3a
okaoka <mnfrf593@yahoo.co.jp>
2014-11-05 08:01:44 +0900

    TLE (1.13s)

--- Volume_10/1032_Course_Planning_for_Lazy_Students.cpp
+++ Volume_10/1032_Course_Planning_for_Lazy_Students.cpp
@@ -30,60 +30,45 @@ static const int ty[] = {-1,0,1,0};
 int prior[21];
 int credit[21];
 int dp[1<<20];
+int bitcount[1<<20];
 int total_subjects;
 int credit_requirement;
 
-class State {
-public:
-  int S;
-  int sum;
-  bool operator<(const State& s) const {
-    return __builtin_popcount(S) < __builtin_popcount(s.S);
-  }
-  bool operator>(const State& s) const {
-    return __builtin_popcount(S) > __builtin_popcount(s.S);
-  }
-  State(int S,int sum) : S(S),sum(sum) {}
-};
-
-int bfs(){
-  priority_queue<State,vector<State>,greater<State> > que;
-  que.push(State(0,0));
-  while(!que.empty()){
-    State s = que.top();
-    que.pop();
-    for(int i = 0; i < total_subjects; i++){
-      if(s.S & (1<<i)) continue;
-      if((prior[i] | s.S) == s.S){
-	if(dp[s.S | (1<<i)] != -1) continue;
-	dp[s.S | (1<<i)] = s.sum + credit[i];
-
-	if(s.sum + credit[i] >= credit_requirement) {
-	  return __builtin_popcount(s.S) + 1;
-	}
-	que.push(State(s.S | (1<<i),s.sum + credit[i]));
-      }
+void dfs(int S,int sum){
+  for(int i = 0; i < total_subjects; ++i){
+    if((prior[i] | S) == S){
+      if(dp[S | (1<<i)] != -1) continue;
+      dp[S | (1<<i)] = sum + credit[i];
+      dfs(S | (1<<i),sum + credit[i]);
     }
   }
-  return 0;
 }
 
 int main(){
+  for(int S = 0; S < (1<<20); ++S){
+    bitcount[S] = __builtin_popcount(S);
+  }
+
   while(~scanf("%d %d",&total_subjects,&credit_requirement)){
     if(total_subjects == 0 && credit_requirement == 0) break;
     memset(dp,-1,sizeof(dp));
     memset(prior,0,sizeof(prior));
 
-    for(int subject_i = 0; subject_i < total_subjects; subject_i++){
+    for(int subject_i = 0; subject_i < total_subjects; ++subject_i){
       int prior_subject_num;
       scanf("%d %d",&credit[subject_i],&prior_subject_num);
-      for(int subject_j = 0; subject_j < prior_subject_num; subject_j++){
+      for(int subject_j = 0; subject_j < prior_subject_num; ++subject_j){
 	int subject_code;
 	scanf("%d",&subject_code);
 	prior[subject_i] |= (1<<subject_code);
       }
     }
-    
-    printf("%d\n",bfs());
+    dfs(0,0);
+    int res = INF;
+    for(int S = 0; S < (1<<total_subjects); ++S){
+      if(dp[S] < credit_requirement) continue;
+      res = min(bitcount[S],res);
+    }
+    printf("%d\n",res);
   }
 }
