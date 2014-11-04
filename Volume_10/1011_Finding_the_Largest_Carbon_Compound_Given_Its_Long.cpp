2ef35bc0ea94c872986e3e3bed67037473c693a7
okaoka <mnfrf593@yahoo.co.jp>
2014-11-04 21:06:47 +0900

    solve 1011_Finding_the_Largest_Carbon_Compound_Given_Its_Long

--- Volume_10/1011_Finding_the_Largest_Carbon_Compound_Given_Its_Long.cpp
+++ Volume_10/1011_Finding_the_Largest_Carbon_Compound_Given_Its_Long.cpp
@@ -27,34 +27,18 @@ static const double EPS = 1e-8;
 static const int tx[] = {0,1,0,-1};
 static const int ty[] = {-1,0,1,0};
 
+int sum[50];
+
 int main(){
-  int n;
-  int idx = 1;
-  int ans[30000];
-  memset(ans,0x3f,sizeof(ans));
-  for(int i = 1,len=1; i <= 30; i += 2){
-    for(int j = 0; j < i; j++){
-      ans[idx] = len;
-      idx++;
-    }
-    len++;
-    for(int j = 0; j < i; j++){
-      ans[idx] = len;
-      idx++;
-    }
-    len++;
+  for(int i = 1,len=1; len <= 30; i *= 3){
+    sum[len] = sum[len-1] + i;
+    sum[len+1] = sum[len] + i;
+    len+=2;
   }
 
+  int n;
   while(~scanf("%d",&n)){
     int res = 0;
-    for(int i = 1; i<=30000;i++){
-      if(ans[i] <= n){
-	res = i;
-      }
-      else{
-	break;
-      }
-    }
-    printf("%d\n",res);
+    printf("%d\n",sum[n]);
   }
 }
