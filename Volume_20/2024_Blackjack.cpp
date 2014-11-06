bd005a0e57e9f85ee62a144365a0f6896e0a5074
okaoka <mnfrf593@yahoo.co.jp>
2014-11-06 23:20:39 +0900

    solve 2024_Blackjack

--- Volume_20/2024_Blackjack.cpp
+++ Volume_20/2024_Blackjack.cpp
@@ -26,12 +26,30 @@ static const double EPS = 1e-8;
 
 static int cards[256];
 
+int compute_score(bool& has_ace11,const vector<string>& hands){
+  int sum = 0;
+
+  bool has_ace = false;
+  for(int i = 0; i < hands.size();i++){
+    int num = cards[hands[i][0]];
+    if(num == 1) has_ace = true;
+    sum += num;
+  }
+  if(has_ace){
+    if(sum + 10 <= 21){
+      has_ace11 = true;
+      sum += 10;
+    }
+  }
+  return sum;
+}
+
 int main(){
   int total_testcases;
-  for(int i = 1; i < 10; i++){
+  for(int i = 2; i < 10; i++){
     cards['0' + i] = i;
   }
-  cards['A'] = 11;
+  cards['A'] = 1;
   cards['T'] = 10;
   cards['J'] = 10;
   cards['Q'] = 10;
@@ -40,54 +58,34 @@ int main(){
   while(~scanf("%d",&total_testcases)){
     for(int test_i = 0; test_i < total_testcases; test_i++){
       string lhs,rhs;
+      vector<string> hands;
       cin >> lhs >> rhs;
-      int numA = cards[lhs[0]];
-      int numB = cards[rhs[0]];
-      
-      int sum = numA + numB;
-      bool has_ace;
-      bool is_blackjack = false;
-      bool is_ace1 = false;
-      if(numA + numB == 21){
-	is_blackjack = true;
-      }
-      if(numA == 11 || numB == 11){
-	has_ace = true;
-	is_ace1 = false;
-      }
 
+      bool is_ace11 = false;
+      hands.push_back(lhs);
+      hands.push_back(rhs);
+
+      int sum = 0;
       for(int pile_i = 0; pile_i < 8; pile_i++){
+
 	string pile;
 	cin >> pile;
-	if(is_blackjack) continue;
+	bool is_ace11 = false;
+	sum = compute_score(is_ace11,hands);
 
-	int num = cards[pile[0]];
 	if(sum <= 16){
-	   // || (sum > 16 && has_ace && !is_ace1 && sum - 10 <= 16)){
-	  if(num == 11){
-	    has_ace = true;
-
-	    if(sum + num > 21){
-	      is_ace1 = true;
-	      sum += 1;
-	    }
-	    else{
-	      sum += num;
-	    }
-	  }
-	  else{
-	    sum += num;
-	  }
+	  hands.push_back(pile);
 	}
-	else if(sum == 17 && is_ace1){
-	  sum += num;
+	else if(sum == 17 && is_ace11){
+	  hands.push_back(pile);
 	}
       }
+      sum = compute_score(is_ace11,hands);
       
       if(sum > 21){
 	printf("bust\n");
       }
-      else if(is_blackjack){
+      else if(sum == 21 && hands.size() == 2){
 	printf("blackjack\n");
       }
       else{
