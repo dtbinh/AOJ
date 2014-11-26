4e06e58ea3d66e50b7a3e800571c78853ea9cd0c
okaoka <mnfrf593@yahoo.co.jp>
2014-11-26 22:48:09 +0900

    WA

--- Volume_20/2025_Eight_Princes.cpp
+++ Volume_20/2025_Eight_Princes.cpp
@@ -39,9 +39,38 @@ ll fact(ll n)
   return (n <= 1 ? 1 : n * fact(n-1));
 }
 
+ll dp[10][101][2][2];
+ll seats;
+
+ll dfs(int used,int pos,int current,int next){
+  if(used >= 8) return 1;
+  if(pos >= seats/2) return 0;
+  if(dp[used][pos][current][next] != -1){
+    return dp[used][pos][current][next];
+  }
+
+  ll res = 0;
+  res += dfs(used,pos+1,0,0);
+  if(current == 0) res += dfs(used+1,pos+1,1,0);
+  if(next == 0 && pos + 1 < seats/2) res += dfs(used+1,pos+1,0,1);
+  return (dp[used][pos][current][next] = res);
+}
+
 int main(){
-  int seats;
-  while(~scanf("%d",&seats)){
+  while(~scanf("%lld",&seats)){
     if(seats == 0) break;
+
+    if(seats % 2 != 0){
+      memset(dp,-1,sizeof(dp));
+      printf("%lld\n",dfs(1,0,1,0) * seats * fact(8 - 1));
+    }
+    else{
+      if(seats <= 16){
+	printf("%lld\n",0);
+      }
+      else{
+	printf("%lld\n",seats * nCr(seats/2,8) * fact(8 - 1));
+      }
+    }
   }
 }
