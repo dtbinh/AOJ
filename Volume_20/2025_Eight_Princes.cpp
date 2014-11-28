6451e32596d32013967d99589d657d6a7193fbac
okaoka <mnfrf593@yahoo.co.jp>
2014-11-29 01:41:36 +0900

    solve 2025_Eight_Princes

--- Volume_20/2025_Eight_Princes.cpp
+++ Volume_20/2025_Eight_Princes.cpp
@@ -30,47 +30,59 @@ int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
+ll _nCr[300][300];
 ll nCr(int n,int r){
- return (r==0 || n==r) ? 1 : nCr(n-1,r) + nCr(n-1,r-1);
+  if(_nCr[n][r] != -1) return _nCr[n][r];
+  return (r==0 || n==r) ? 1 : (_nCr[n][r] = nCr(n-1,r) + nCr(n-1,r-1));
 }
+
+ll _fact[300];
 
 ll fact(ll n)
 {
-  return (n <= 1 ? 1 : n * fact(n-1));
+  if(_fact[n] != -1) return _fact[n];
+  return (n <= 1 ? 1 : (_fact[n] = n * fact(n-1)));
 }
 
-ll dp[10][101][2][2];
+struct Comparer {
+  bool operator() (const bitset<71> &b1, const bitset<71> &b2) const {
+    return b1.to_ulong() < b2.to_ulong();
+  }
+};
+
+ll dp[101][10][2][2];
 ll seats;
 
-ll dfs(int used,int pos,int current,int next){
+ll dfs(int pos,int used,bool here,bool opposite){
   if(used >= 8) return 1;
   if(pos >= seats/2) return 0;
-  if(dp[used][pos][current][next] != -1){
-    return dp[used][pos][current][next];
-  }
+  if(dp[pos][used][here][opposite] != -1) return dp[pos][used][here][opposite];
+  if(pos + 1 == seats/2) here = false;
 
   ll res = 0;
-  res += dfs(used,pos+1,0,0);
-  if(current == 0) res += dfs(used+1,pos+1,1,0);
-  if(next == 0 && pos + 1 < seats/2) res += dfs(used+1,pos+1,0,1);
-  return (dp[used][pos][current][next] = res);
+  res += dfs(pos+1,used,true,true);
+  if(here) res += dfs(pos+1,used + 1,false,true);
+  if(opposite) res += dfs(pos+1,used + 1,true,false);
+
+  return (dp[pos][used][here][opposite] = res);
 }
 
 int main(){
+
+  memset(_nCr,-1,sizeof(_nCr));
+  memset(_fact,-1,sizeof(_fact));
   while(~scanf("%lld",&seats)){
     if(seats == 0) break;
 
-    if(seats % 2 != 0){
-      memset(dp,-1,sizeof(dp));
-      printf("%lld\n",dfs(1,0,1,0) * seats * fact(8 - 1));
+    if(seats <= 16){
+      printf("%d\n",0);
+    }
+    else if(seats % 2 != 0){
+      printf("%lld\n",seats * nCr(seats - 9,7) * fact(7));
     }
     else{
-      if(seats <= 16){
-	printf("%lld\n",0);
-      }
-      else{
-	printf("%lld\n",seats * nCr(seats/2,8) * fact(8 - 1));
-      }
+      memset(dp,-1,sizeof(dp));
+      printf("%lld\n",seats * dfs(1,1,false,true) * fact(7));
     }
   }
 }
