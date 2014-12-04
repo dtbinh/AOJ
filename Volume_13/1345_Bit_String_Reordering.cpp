a93e5ac48db602fb74e4cd67ea072ec4ec5bfc7e
okaoka <mnfrf593@yahoo.co.jp>
2014-12-04 23:21:25 +0900

    solve 1345_Bit_String_Reordering

--- Volume_13/1345_Bit_String_Reordering.cpp
+++ Volume_13/1345_Bit_String_Reordering.cpp
@@ -29,15 +29,31 @@ typedef pair <int,P > PP;
 typedef complex<double> Point;
 const static double EPS = 1e-12;
 
+class State {
+public:
+  int bits;
+  int cost;
+  State(int bits,int cost) :
+    bits(bits), cost(cost) {}
+  bool operator<(const State& s) const {
+    return cost < s.cost;
+  }
+  bool operator>(const State& s) const {
+    return cost > s.cost;
+  }
+};
+
+int dp[1<<15];
+
 int main(){
   int bit_length;
   int run_length;
   while(~scanf("%d %d",&bit_length,&run_length)){
-    int bits = 0;
+    int init = 0;
     for(int i = 0; i < bit_length; i++){
       int bit;
       scanf("%d",&bit);
-      if(bit > 0) bits |= (1<<i);
+      if(bit > 0) init |= (1<<i);
     }
     
     vector<int> width;
@@ -47,6 +63,37 @@ int main(){
       width.push_back(run);
     }
 
+    priority_queue<State,vector<State>,greater<State> > que;
+    que.push(State(init,0));
+    memset(dp,0x3f,sizeof(dp));
+    dp[init] = 0;
+
+    while(!que.empty()){
+      State s = que.top();
+      que.pop();
+      for(int i = 0; i + 1 < bit_length; i++){
+	int j = i + 1;
+	int next = s.bits;
+	if((s.bits & (1<<i)) && !(s.bits & (1<<j))){
+	  next &= ~(1<<i);
+	  next |= (1<<j);
+	  if(dp[next] > s.cost + 1){
+	    dp[next] = s.cost + 1;
+	    que.push(State(next,s.cost + 1));
+	  }
+	}
+	else if(!(s.bits & (1<<i)) && (s.bits & (1<<j))){
+	  next &= ~(1<<j);
+	  next |= (1<<i);
+	  if(dp[next] > s.cost + 1){
+	    dp[next] = s.cost + 1;
+	    que.push(State(next,s.cost + 1));
+	  }
+	}
+      }
+    }
+    
+    int res = INF;
     for(int S = 0; S < (1<<bit_length); S++){
       int offset = 0;
       bool isok = true;
@@ -66,11 +113,10 @@ int main(){
       }
 
       if(isok){
-	for(int i = 0; i < bit_length; i++){
-	  printf("%d",(S & (1<<i)) ? 1 : 0);
-	}
-	printf("\n");
+	res = min(dp[S],res);
       }
     }
+
+    printf("%d\n",res);
   }
 }
