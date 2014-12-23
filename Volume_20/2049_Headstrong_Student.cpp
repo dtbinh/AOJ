6403914653da36ec743f03e487c98bf067366863
okaoka <mnfrf593@yahoo.co.jp>
2014-12-23 14:31:17 +0900

    WA

--- Volume_20/2049_Headstrong_Student.cpp
+++ Volume_20/2049_Headstrong_Student.cpp
@@ -30,12 +30,38 @@ int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
+int visited[1000001];
+
 int main(){
   int dividend;
   int divisor;
   while(~scanf("%d %d",&dividend,&divisor)){
     if(divisor == 0) break;
-
     
+    memset(visited,-1,sizeof(visited));
+
+    int pos = 0;
+    int len = 0;
+    for(int i = 0; i < 10; i++){
+      // cout << "i" << i << " ";
+      if(dividend % divisor == 0){
+	// cout << dividend / divisor << endl;
+	pos = i;
+	len = 0;
+	break;
+      }
+      // cout << dividend / divisor << endl;
+      // cout << dividend % divisor << endl;
+      if(visited[dividend % divisor] != -1){
+	pos = visited[dividend % divisor];
+	len = i - visited[dividend % divisor];
+	break;
+      }
+      visited[dividend % divisor] = i;
+      dividend %= divisor;
+      dividend *= 10;
+    }
+
+    printf("%d %d\n",pos,len);
   }
 }
