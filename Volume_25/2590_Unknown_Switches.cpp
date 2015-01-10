b7db94971701644209f37b66e2d1af64cd3231f7
okaoka <mnfrf593@yahoo.co.jp>
2015-01-10 15:50:12 +0900

    solve 2590_Unknown_Switches

--- Volume_25/2590_Unknown_Switches.cpp
+++ Volume_25/2590_Unknown_Switches.cpp
@@ -41,10 +41,10 @@ int main(){
 	       &num_of_operations)){
     if(num_of_switches == 0
        && num_of_light_bulbs == 0
-       && num_of_operations) break;
+       && num_of_operations == 0) break;
 
     bool flags[50] = {};
-    bool states[50][50] = {};
+    bool states[50][1001] = {};
     vector<string> light_bulbs_log;
     for(int operation_i = 0; operation_i < num_of_operations; operation_i++){
       string switches;
@@ -61,8 +61,8 @@ int main(){
     
     for(int light_i = 0; light_i < num_of_light_bulbs; light_i++){
 
+      vector<int> candidates;
       for(int switch_i = 0; switch_i < num_of_switches; switch_i++){
-	bool is_pair = false;
 	bool isok = true;
 	for(int operation_i = 0; operation_i < light_bulbs_log.size(); operation_i++){
 	  bool state = (light_bulbs_log[operation_i][light_i] == '1' ? true : false);
@@ -73,24 +73,19 @@ int main(){
 	}
 
 	if(isok){
-	  is_pair = true;
+	  candidates.push_back(switch_i);
 	}
-	
-	isok = true;
-	for(int operation_i = 0; operation_i < light_bulbs_log.size(); operation_i++){
-	  bool state = (light_bulbs_log[operation_i][light_i] == '1' ? true : false);
-	  if(states[switch_i][operation_i] == state){
-	    isok = false;
-	    break;
-	  }
-	}
-      
-	if(isok){
-	  is_pair = true;
-	}
+      }
 
-	if(is_pair){
-	  printf("%d %d\n",light_i,switch_i);
+      if(candidates.size() != 1){
+	printf("?");
+      }
+      else{
+	if(candidates[0] >= 10){
+	  printf("%c",candidates[0] - 10 + 'A');
+	}
+	else{
+	  printf("%c",candidates[0] + '0');
 	}
       }
     }
