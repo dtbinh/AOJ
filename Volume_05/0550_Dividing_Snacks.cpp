34fee05530c3b9507a7c28182c227fd896395e19
okaoka <mnfrf593@yahoo.co.jp>
2015-01-19 23:13:45 +0900

    wrote input portion

---
+++ Volume_05/0550_Dividing_Snacks.cpp
@@ -0,0 +1,43 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int N;
+  while(~scanf("%d",&N)){
+    vector<int> costs;
+    for(int i = 0; i < N; i++){
+      int cost;
+      scanf("%d",&cost);
+      costs.push_back(cost);
+    }
+  }
+}
