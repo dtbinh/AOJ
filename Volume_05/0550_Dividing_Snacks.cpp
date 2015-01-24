1be20a24d3bf6a5aa02ba0854f591073f7cfe965
okaoka <mnfrf593@yahoo.co.jp>
2015-01-24 23:14:48 +0900

    WA

--- Volume_05/0550_Dividing_Snacks.cpp
+++ Volume_05/0550_Dividing_Snacks.cpp
@@ -30,14 +30,28 @@ static const int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
+int dp[2][10001][2];
+
 int main(){
   int N;
   while(~scanf("%d",&N)){
     vector<int> costs;
-    for(int i = 0; i < N; i++){
+    for(int i = 0; i < N - 1; i++){
       int cost;
       scanf("%d",&cost);
       costs.push_back(cost);
     }
+
+    memset(dp,0x3f,sizeof(dp));
+    dp[0][0][1] = dp[0][0][0] = 0;
+    for(int pos = 0; pos < N; pos++){
+      for(int num_of_zero = pos; num_of_zero >= 0; num_of_zero--){
+	dp[(pos+1) % 2][num_of_zero+1][0]
+	  = min(dp[pos % 2][num_of_zero+1][0],dp[pos % 2][num_of_zero][1] + costs[pos]);
+	dp[(pos+1) % 2][num_of_zero][1]
+	  = min(dp[pos % 2][num_of_zero][1],dp[pos % 2][num_of_zero][0] + costs[pos]);
+      }
+    }
+    printf("%d\n",dp[0][N / 2][0]);
   }
 }
