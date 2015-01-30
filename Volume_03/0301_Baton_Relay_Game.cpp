1023d4b8406fbcd7d392eb19c7de045aa0d7e29c
okaoka <mnfrf593@yahoo.co.jp>
2015-01-30 23:28:43 +0900

    added input portion

---
+++ Volume_03/0301_Baton_Relay_Game.cpp
@@ -0,0 +1,50 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+const static int tx[] = {0,1,0,-1};
+const static int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int total_students;
+  int total_repeat_count;
+  int total_questions;
+  while(~scanf("%d %d %d",
+	       &total_students,
+	       &total_repeat_count,
+	       &total_questions)){
+    for(int repeat_i = 0; repeat_i < total_repeat_count; repeat_i++){
+      int target;
+      scanf("%d",&target);
+    }
+    for(int question_i = 0; question_i < total_questions; question_i++){
+      int query;
+      scanf("%d",&query);
+    }
+  }
+}
