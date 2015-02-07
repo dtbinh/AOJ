563801b270ebfc18f726bdc13311f8a37a7e0bd5
okaoka <mnfrf593@yahoo.co.jp>
2015-02-08 00:38:46 +0900

    output of the test case3 is wrong

--- Volume_02/0299_Railroad_II.cpp
+++ Volume_02/0299_Railroad_II.cpp
@@ -23,6 +23,30 @@ typedef pair<int,string> P;
 
 static const double EPS = 1e-8;
 
+
+int compute_cost(int p1,int p2,int dept_station,int total_stations,
+		 const vector<int>& stations){
+  
+  int res = INF;
+  if(stations[p1] == dept_station 
+     || stations[p2] == dept_station){
+    int dist1 = abs(stations[p2] - stations[p1]);
+    int dist2 = total_stations - abs(stations[p2] - stations[p1]);
+    res = 100 * min(dist1 * 2 + dist2,dist1 + dist2 * 2);
+  }
+  else if(stations[p1] > dept_station){
+    int dist1 = abs(dept_station - stations[p1]);
+    int dist2 = (total_stations - stations[p2]) + dept_station;
+    res = 100 * min(dist1 * 2 + dist2,dist1 + dist2 * 2);
+  }
+  else if(stations[p1] < dept_station){
+    int dist1 = abs(dept_station - stations[p2]);
+    int dist2 = (total_stations - dept_station) + stations[p1];
+    res = 100 * min(dist1 * 2 + dist2,dist1 + dist2 * 2);
+  }
+
+  return res;
+}
 int main(){
   int total_stations;
   int total_buy_stations;
@@ -37,11 +61,19 @@ int main(){
       scanf("%d",&station_id);
       stations.push_back(station_id);
     }
+
     sort(stations.begin(),stations.end());
 
-    int res = 0;
-    for(int i = 0; i + 1 < stations.size(); i++){
-      res += (stations[i+1] - stations[i]) * 100;
+    int res = INF;
+
+    if(dept_station < stations[0]){
+      res = 100 * (stations[stations.size() - 1] - dept_station);
+    }
+
+    for(int i = 0; i < stations.size(); i++){
+      int p1 = i;
+      int p2 = (i + 1) % stations.size();
+      res = min(compute_cost(p1,p2,dept_station,total_stations,stations),res);
     }
     printf("%d\n",res);
   }
