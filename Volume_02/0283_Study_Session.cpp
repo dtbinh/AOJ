33e4f15accb3b962ebd40a3db8e490dd7c997fda
okaoka <mnfrf593@yahoo.co.jp>
2015-02-21 12:28:45 +0900

    updated

---
+++ Volume_02/0283_Study_Session.cpp
@@ -0,0 +1,98 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+
+int main(){
+  int total_students;
+  int total_queries;
+  while(~scanf("%d %d",&total_students,&total_queries)){
+    vector<int> scores;
+    for(int student_i = 0; student_i < total_students; student_i++){
+      int score;
+      scanf("%d",&score);
+      scores.push_back(score);
+    }
+    vector<int> leaders;
+    for(int query_i = 0; query_i < total_queries; query_i++){
+      string operation;
+      int num;
+      cin >> operation >> num;
+      if(operation == "ADD"){
+	leaders.push_back(num - 1);
+      }
+      else if(operation == "REMOVE"){
+	leaders.erase(lower_bound(leaders.begin(),leaders.end(),num - 1));
+      }
+      else if(operation == "CHECK"){
+	vector<int> leader_scores;
+
+	for(int leader_i = 0; leader_i < leaders.size(); leader_i++){
+	  leader_scores.push_back(scores[leaders[leader_i]]);
+	}
+
+	priority_queue<int> diff_log;
+	for(int score_i = 0; score_i < scores.size(); score_i++){
+	  if(!binary_search(leaders.begin(),leaders.end(),score_i)){
+	    int score = scores[score_i];
+	    int pos = lower_bound(leader_scores.begin(),leader_scores.end(),score) - leader_scores.begin();
+	    if(pos == leader_scores.size()){
+	      diff_log.push(INF);
+	    }
+	    else if(pos == 0 || leader_scores[pos] == score){
+	      int diff = leader_scores[pos] - score;
+	      diff_log.push(diff);
+	    }
+	    else if(pos < leader_scores.size()){	    
+	      int diff = leader_scores[pos - 1] - score;
+	      diff_log.push(diff);
+	    }
+	  }
+	}
+	while(num > 0 && !diff_log.empty()){
+	  diff_log.pop();
+	  num--;
+	}
+	if(!diff_log.empty()){
+	  if(diff_log.top() == INF){
+	    printf("NA\n");
+	  }
+	  else{
+	    printf("%d\n",diff_log.top());
+	  }
+	}
+	else{
+	  printf("%d\n",0);
+	}
+      }
+    }
+  }
+}
