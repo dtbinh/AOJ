3ec510744872a3c1a5e3c9f2636aa67594a0bd5f
okaoka <mnfrf593@yahoo.co.jp>
2015-03-01 15:13:22 +0900

    added input portion

---
+++ Volume_02/0211_Jogging.cpp
@@ -0,0 +1,43 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+
+static const int tx[] = {+0,+1,+0,-1};
+static const int ty[] = {-1,+0,+1,+0};
+
+int main(){
+  int total_students;
+  while(~scanf("%d",&total_students)){
+    for(int student_i = 0; student_i < total_students; student_i++){
+      int distance;
+      int velocity;
+      scanf("%d %d",&distance,&velocity);
+    }
+  }
+}
