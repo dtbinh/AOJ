dc6f76cc2ff62d4f5115067020a65384ec4255e3
okaoka <mnfrf593@yahoo.co.jp>
2015-03-01 18:36:55 +0900

    solve 0211_Jogging

--- Volume_02/0211_Jogging.cpp
+++ Volume_02/0211_Jogging.cpp
@@ -31,13 +31,55 @@ static const double EPS = 1e-8;
 static const int tx[] = {+0,+1,+0,-1};
 static const int ty[] = {-1,+0,+1,+0};
 
+ll GCD(ll a ,ll b){
+  return b > 0 ? GCD(b,a%b) : a;
+}
+
+ll LCM(ll a ,ll b){
+  return a / GCD(a,b) * b;
+}
+
+class Student {
+public:
+  ll distance;
+  ll velocity;
+  Student(ll distance,ll velocity) 
+    : distance(distance), velocity(velocity) {}
+};
+
 int main(){
   int total_students;
   while(~scanf("%d",&total_students)){
+    if(total_students == 0) break;
+
+    vector<Student> students;
     for(int student_i = 0; student_i < total_students; student_i++){
-      int distance;
-      int velocity;
-      scanf("%d %d",&distance,&velocity);
+      ll distance;
+      ll velocity;
+      scanf("%lld %lld",&distance,&velocity);
+      int gcd = GCD(distance,velocity);
+      students.push_back(Student(distance / gcd,velocity / gcd));
+    }
+
+    ll reference_distance = students[0].distance;
+    for(int student_i = 0; student_i < total_students; student_i++){
+      reference_distance = LCM(reference_distance,
+			       students[student_i].distance);
+
+    }
+
+    for(int student_i = 0; student_i < total_students; student_i++){
+      students[student_i].velocity
+	= students[student_i].velocity * (reference_distance / students[student_i].distance);
+    }
+
+    ll gcd = students[0].velocity;
+    for(int student_i = 0; student_i < total_students; student_i++){
+      gcd = GCD(gcd,students[student_i].velocity);
+    }
+	
+    for(int student_i = 0; student_i < total_students; student_i++){
+      printf("%lld\n",students[student_i].velocity / gcd);
     }
   }
 }
