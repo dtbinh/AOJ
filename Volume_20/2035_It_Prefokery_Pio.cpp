80bdb573ab7525732aba5178cfaa9a661b2ec9f6
okaoka <mnfrf593@yahoo.co.jp>
2015-03-09 01:02:04 +0900

    added input portion

---
+++ Volume_20/2035_It_Prefokery_Pio.cpp
@@ -0,0 +1,39 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+#include <complex>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-10;
+
+int main(){
+  string str;
+  while(cin >> str){
+    
+  }
+}
