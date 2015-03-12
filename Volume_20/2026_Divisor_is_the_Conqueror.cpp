be2f848cb07c8e4426a41f590d29cb007015bb25
okaoka <mnfrf593@yahoo.co.jp>
2015-03-12 23:25:05 +0900

    MLE

--- Volume_20/2026_Divisor_is_the_Conqueror.cpp
+++ Volume_20/2026_Divisor_is_the_Conqueror.cpp
@@ -31,25 +31,34 @@ static const int ty[] = {-1,0,1,0};
 static const double EPS = 1e-8;
 
 map<vector<int>,bool> visited;
+vector<int> answer;
 
-void dfs(int sum,int total_rank,vector<int>& cards){
+bool dfs(int sum,int total_rank,vector<int>& cards,vector<int>& order){
   if(sum == total_rank){
-    printf("ok\n");
+    answer = order;
+    return true;
   }
-  if(visited.find(cards) != visited.end()) return;
+  bool res = false;
+  if(visited.find(cards) != visited.end()) return false;
+
   visited[cards] = true;
   for(int i = 1; i <= 13; i++){
     if(cards[i] > 0 && sum % i == 0){
       cards[i]--;
-      dfs(sum + i,total_rank,cards);
+      order.push_back(i);
+      res |= dfs(sum + i,total_rank,cards,order);
+      order.pop_back();
       cards[i]++;
     }
   }
+  return res;
 }
 
 int main(){
   int num_of_cards;
   while(~scanf("%d",&num_of_cards)){
+    if(num_of_cards == 0) break;
+
     vector<int> cards(14);
     int total_rank = 0;
     for(int i = 0; i < num_of_cards; i++){
@@ -58,6 +67,19 @@ int main(){
       cards[rank]++;
       total_rank += rank;
     }
-    dfs(0,total_rank,cards);
+    vector<int> order;
+    bool isok = dfs(0,total_rank,cards,order);
+    bool is_first = true;
+
+    if(isok){
+      for(int i = 0; i < answer.size(); i++){
+	printf("%s%d",is_first ? "" : " ",answer[i]);
+	is_first = false;
+      }
+      printf("\n");
+    }
+    else{
+      printf("No\n");
+    }
   }
 }
