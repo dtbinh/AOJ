17075a6e31b78d00aaabe2d336a5bdaba730dc15
okaoka <mnfrf593@yahoo.co.jp>
2015-03-12 23:03:03 +0900

    updated

--- Volume_20/2026_Divisor_is_the_Conqueror.cpp
+++ Volume_20/2026_Divisor_is_the_Conqueror.cpp
@@ -30,12 +30,34 @@ static const int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
+map<vector<int>,bool> visited;
+
+void dfs(int sum,int total_rank,vector<int>& cards){
+  if(sum == total_rank){
+    printf("ok\n");
+  }
+  if(visited.find(cards) != visited.end()) return;
+  visited[cards] = true;
+  for(int i = 1; i <= 13; i++){
+    if(cards[i] > 0 && sum % i == 0){
+      cards[i]--;
+      dfs(sum + i,total_rank,cards);
+      cards[i]++;
+    }
+  }
+}
+
 int main(){
   int num_of_cards;
   while(~scanf("%d",&num_of_cards)){
-    int ranks[52];
+    vector<int> cards(14);
+    int total_rank = 0;
     for(int i = 0; i < num_of_cards; i++){
-      scanf("%d",rank + i);
+      int rank;
+      scanf("%d",&rank);
+      cards[rank]++;
+      total_rank += rank;
     }
+    dfs(0,total_rank,cards);
   }
 }
