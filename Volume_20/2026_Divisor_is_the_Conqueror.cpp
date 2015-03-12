f1a02864909a18b5799eb548bb626f28c83026ab
okaoka <mnfrf593@yahoo.co.jp>
2015-03-12 22:12:09 +0900

    added input portion

---
+++ Volume_20/2026_Divisor_is_the_Conqueror.cpp
@@ -0,0 +1,41 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int num_of_cards;
+  while(~scanf("%d",&num_of_cards)){
+    int ranks[52];
+    for(int i = 0; i < num_of_cards; i++){
+      scanf("%d",rank + i);
+    }
+  }
+}
