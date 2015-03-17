ccdeda466e18be6ec5627dd1ea32b278925d0efb
okaoka <mnfrf593@yahoo.co.jp>
2015-03-18 00:56:30 +0900

    MLE

--- Volume_12/1208_Rational_Irrationals.cpp
+++ Volume_12/1208_Rational_Irrationals.cpp
@@ -25,14 +25,63 @@ using namespace std;
   
 typedef long long ll;
 typedef pair <int,int> P;
-typedef pair <int,P> PP;
+typedef pair <double,P> PP;
   
 static const double EPS = 1e-8;
 
+int gcd(int a, int b)
+{
+  if(b == 0) return a;
+  return gcd(b, a % b);
+}
+
+class Rational {
+public:
+  double value;
+  int x;
+  int y;
+  Rational(double value,int x,int y)
+    : value(value),x(x),y(y) {}
+  bool operator<(const Rational& r) const{
+    return value < r.value;
+  }
+  bool operator>(const Rational& r) const{
+    return value > r.value;
+  }
+  bool operator>(const double v) const{
+    return value > v;
+  }
+  bool operator<(const double v) const{
+    return value < v;
+  }
+};
+
 int main(){
-  int p,n;
-  while(~scanf("%d %d",&p,&n)){
+  double p;
+  int n;
+
+  while(~scanf("%lf %d",&p,&n)){
     if(p == 0 && n == 0) break;
+
+    vector<Rational> rational_numbers;
+    for(int numerator = 1; numerator <= n; numerator++){
+      for(int denominator = 1; denominator <= numerator; denominator++){
+	int div = gcd(numerator,denominator);
+	int x = numerator / div;
+	int y = denominator / div;
+	if(x != numerator || y != denominator) continue;
+	rational_numbers.push_back(Rational((double)x/(double)y,x,y));
+      }
+    }
+    sort(rational_numbers.begin(),rational_numbers.end());
+
+    int i = lower_bound(rational_numbers.begin(),rational_numbers.end(),sqrt(p)) - rational_numbers.begin();
+    i--;
+    int u = rational_numbers[i].x;
+    int v = rational_numbers[i].y;
     
+    int x = rational_numbers[i+1].x;
+    int y = rational_numbers[i+1].y;
+    printf("%d/%d %d/%d\n",x,y,u,v);
   }
 }
