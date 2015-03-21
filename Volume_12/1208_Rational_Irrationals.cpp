48facf10d0292a1c6f4161a0605e76a14fb43f10
okaoka <mnfrf593@yahoo.co.jp>
2015-03-21 14:59:40 +0900

    solve 1208_Rational_Irrationals

--- Volume_12/1208_Rational_Irrationals.cpp
+++ Volume_12/1208_Rational_Irrationals.cpp
@@ -29,38 +29,53 @@ typedef pair <double,P> PP;
   
 static const double EPS = 1e-8;
 
-int gcd(int a, int b)
-{
-  if(b == 0) return a;
-  return gcd(b, a % b);
-}
-
 class SternBrocotTree {
 public:
-  SternBrocotTree(int B,int p) {
+  SternBrocotTree(int B,int p)
+    : max_depth(0),
+      ans_left_numerator(0),
+      ans_right_numerator(0),
+      ans_left_denominator(0),
+      ans_right_denominator(0){
     this->B = B;
     this->p = p;
   }
 
   void dfs(int left_numerator,int left_denominator,
-	   int right_numerator,int right_denominator){
+	   int right_numerator,int right_denominator,int depth){
     int next_numerator = left_numerator + right_numerator;
     int next_denominator = left_denominator + right_denominator;
 
     if(contains(left_numerator,left_denominator,next_numerator,next_denominator)){
-      dfs(left_numerator,left_denominator,next_numerator,next_denominator);
+      dfs(left_numerator,left_denominator,next_numerator,next_denominator,depth+1);
     }
-    printf("%d/%d %d/%d\n",
-	   right_numerator,right_denominator,
-	   left_numerator,left_denominator);
+
+    if(max_depth < depth){
+      max_depth = depth;
+      ans_right_numerator = right_numerator;
+      ans_right_denominator = right_denominator;
+      ans_left_numerator = left_numerator;
+      ans_left_denominator = left_denominator;
+    }
+
     if(contains(next_numerator,next_denominator,right_numerator,right_denominator)){
-      dfs(next_numerator,next_denominator,right_numerator,right_denominator);
+      dfs(next_numerator,next_denominator,right_numerator,right_denominator,depth+1);
     }
   }
-  
+  void disp(){
+    printf("%d/%d %d/%d\n",
+	   ans_right_numerator,ans_right_denominator,
+	   ans_left_numerator,ans_left_denominator);
+  }
 private:
   int B;
   int p;
+  int max_depth;
+  int ans_left_numerator;
+  int ans_left_denominator;
+  int ans_right_numerator;
+  int ans_right_denominator;
+  
   bool contains(int left_numerator,int left_denominator,
 		int right_numerator,int right_denominator){
     if(left_numerator > B || left_denominator > B
@@ -80,6 +95,7 @@ int main(){
   while(~scanf("%d %d",&p,&n)){
     if(p == 0 && n == 0) break;
     SternBrocotTree sbt(n,p);
-    sbt.dfs(0,1,1,0);
+    sbt.dfs(0,1,1,0,0);
+    sbt.disp();
   }
 }
