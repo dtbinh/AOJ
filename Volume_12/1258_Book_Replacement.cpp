c3b3722cd4000b568fa83ac742c57ea219f339bd
okaoka <mnfrf593@yahoo.co.jp>
2015-03-25 02:08:12 +0900

    WA

--- Volume_12/1258_Book_Replacement.cpp
+++ Volume_12/1258_Book_Replacement.cpp
@@ -29,12 +29,29 @@ typedef pair <double,P> PP;
   
 static const double EPS = 1e-8;
 
-int main(){
-  int num_of_desks;
-  int book_limit;
-  int num_of_students;
-  while(~scanf("%d %d %d",&num_of_desks,&book_limit,&num_of_students)){
+int num_of_desks;
+int book_limit;
+int num_of_students;
 
+int take_book(int id,deque<int> desks[101]){
+  for(int desk_i = 0; desk_i < num_of_desks; desk_i++){
+    for(int i = 0; i < desks[desk_i].size(); i++){
+      if(desks[desk_i][i] == id){
+	deque<int> tmp;
+	for(int j = 0; j < desks[desk_i].size(); j++){
+	  if(desks[desk_i][j] == id) continue;
+	  tmp.push_back(desks[desk_i][j]);
+	}
+	desks[desk_i] = tmp;
+	return desk_i + 1;
+      }
+    }
+  }
+  return num_of_desks + 1;
+}
+
+int main(){
+  while(~scanf("%d %d %d",&num_of_desks,&book_limit,&num_of_students)){
     vector<int> students[101];
     int max_request = 0;
     for(int student_i = 0; student_i < num_of_students; student_i++){
@@ -61,52 +78,64 @@ int main(){
     while(!books.empty()){
       int id = books.front();
       books.pop();
+      // cout << "book:" << id << endl;
+      //take from desk/shelf
+      score += take_book(id,desks);
+      // cout << "take:" << score << endl;
+      //put 
       if(desks[0].size() < book_limit){
 	desks[0].push_back(id);
-	score += num_of_desks + 1;
 	score += 1;
       }
       else{
 	int tmp_place = -1;
 	for(int desk_i = 1; desk_i <= num_of_desks; desk_i++){
+	  //put to shelf
 	  if(desk_i == num_of_desks){
 	    desks[desk_i].push_back(id);
 	    tmp_place = desk_i;
-	    score += num_of_desks + 1;
+	    score += desk_i + 1;
 	    break;
 	  }
+	  //put to desk
 	  else if(desks[desk_i].size() < book_limit){
 	    desks[desk_i].push_back(id);
 	    tmp_place = desk_i;
-	    score += num_of_desks + 1;
 	    score += desk_i + 1;
 	    break;
 	  }
 	}
 
+	//take book
 	int old = desks[0].front(); 
 	desks[0].pop_front();
 	score += 1;
 
-	bool use_shelf = true;
-	for(int desk_i = 1; desk_i < num_of_desks; desk_i++){
-	  if(desks[desk_i].size() < book_limit){
+	for(int desk_i = 1; desk_i <= num_of_desks; desk_i++){
+	  //put book
+	  if(desk_i == num_of_desks){
 	    desks[desk_i].push_back(old);
 	    score += desk_i + 1;
-	    use_shelf = false;
 	    break;
 	  }
-	}
-	if(use_shelf){
-	  score += num_of_desks + 1;
+	  //put book
+	  else if(desks[desk_i].size() < book_limit){
+	    desks[desk_i].push_back(old);
+	    score += desk_i + 1;
+	    break;
+	  }
+
 	}
 
+	//take book
 	desks[tmp_place].pop_back();
 	score += tmp_place + 1;
 
+	//put book
 	desks[0].push_back(id);
 	score += 1;
       }
+      // cout << score << endl;
     }
     printf("%d\n",score);
   }
