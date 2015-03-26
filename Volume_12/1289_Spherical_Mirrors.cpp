5fda44f3a929ed628f6e7d7fb85e3aba270d10c4
okaoka <mnfrf593@yahoo.co.jp>
2015-03-26 23:10:51 +0900

    added input portion

---
+++ Volume_12/1289_Spherical_Mirrors.cpp
@@ -0,0 +1,42 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+#include <assert.h>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <double,P> PP;
+  
+static const double EPS = 1e-8;
+
+int main(){
+  int num_of_spheres;
+  while(~scanf("%d",&num_of_spheres)){
+    int u,v,w;
+    scanf("%d %d %d",&u,&v,&w);
+    for(int sphere_i = 0; sphere_i < num_of_spheres; sphere_i++){
+      int x,y,z,r;
+      scanf("%d %d %d %d",&x,&y,&z,&r);
+    }
+  }
+}
