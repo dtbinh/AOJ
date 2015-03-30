f7a2ab45622d5bd671952bb25c6ef7a2ff7ad1cb
okaoka <mnfrf593@yahoo.co.jp>
2015-03-30 23:58:06 +0900

    added input portion

---
+++ Volume_12/1298_Separate_Points.cpp
@@ -0,0 +1,46 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+#include <iterator>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {0,1,0,-1};//URDL
+int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int n,m;
+  while(~scanf("%d %d",&n,&m)){
+    for(int i = 0; i < n; i++){
+      int x,y;
+      scanf("%d %d",&x,&y);
+    }
+    for(int i = 0; i < m; i++){
+      int x,y;
+      scanf("%d %d",&x,&y);
+    }
+  }
+}
