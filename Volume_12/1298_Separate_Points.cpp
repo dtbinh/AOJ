6cb00d987e6f75b351a0c2f02347617dd12e2cf6
okaoka <mnfrf593@yahoo.co.jp>
2015-03-31 01:21:57 +0900

    TLE

--- Volume_12/1298_Separate_Points.cpp
+++ Volume_12/1298_Separate_Points.cpp
@@ -23,7 +23,7 @@
 using namespace std;
  
 typedef long long ll;
-typedef pair <int,int> P;
+typedef pair <double,double> P;
 typedef pair <int,P > PP;
  
 int tx[] = {0,1,0,-1};//URDL
@@ -31,16 +31,65 @@ int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
+bool McCullochPitts(const vector<P>& white,const vector<P>& black){
+  vector<double> weight(3);
+  double alpha = 5.0;
+
+  bool res = false;
+  weight[0] = 0.0;
+  weight[1] = 0.0;
+  weight[2] = 0.0;
+
+  for(int round = 0; round < 5000000; round++){
+    bool isok = true;
+    for(int i = 0; i < white.size(); i++){
+      double o = (weight[0] + weight[1] * white[i].first + weight[2] * white[i].second);
+      //white == 0
+      //black == 1
+      if(o > 0){
+	weight[0] -= alpha;
+	weight[1] -= alpha * white[i].first;
+	weight[2] -= alpha * white[i].second;
+	isok = false;
+      }
+    }
+    for(int i = 0;i < black.size(); i++){
+      double o = (weight[0] + weight[1] * black[i].first + weight[2] * black[i].second);
+      //white == 0
+      //black == 1
+      if(o <= 0){
+	weight[0] += alpha;
+	weight[1] += alpha * black[i].first;
+	weight[2] += alpha * black[i].second;
+	isok = false;
+      }
+    }
+    if(isok){
+      res = true;
+      break;
+    }
+  }
+  return res;
+}
+
 int main(){
   int n,m;
   while(~scanf("%d %d",&n,&m)){
+    if(n == 0 && m == 0) break;
+
+    vector<P> white;
     for(int i = 0; i < n; i++){
-      int x,y;
-      scanf("%d %d",&x,&y);
+      double x,y;
+      scanf("%lf %lf",&x,&y);
+      white.push_back(P(x,y));
     }
+    vector<P> black;
     for(int i = 0; i < m; i++){
-      int x,y;
-      scanf("%d %d",&x,&y);
+      double x,y;
+      scanf("%lf %lf",&x,&y);
+      black.push_back(P(x,y));
     }
+
+    printf("%s\n",McCullochPitts(white,black) ? "YES" : "NO");
   }
 }
