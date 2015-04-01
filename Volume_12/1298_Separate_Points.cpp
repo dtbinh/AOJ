d63c8906d256a38b84387b35279f8a9aaed34e20
okaoka <mnfrf593@yahoo.co.jp>
2015-04-01 23:35:27 +0900

    added convex hull

--- Volume_12/1298_Separate_Points.cpp
+++ Volume_12/1298_Separate_Points.cpp
@@ -19,6 +19,8 @@
 #include <cctype>
 #include <utility>
 #include <iterator>
+#include <complex>
+#include <assert.h>
  
 using namespace std;
  
@@ -31,45 +33,98 @@ int ty[] = {-1,0,1,0};
  
 static const double EPS = 1e-8;
 
-bool McCullochPitts(const vector<P>& white,const vector<P>& black){
-  vector<double> weight(3);
-  double alpha = 5.0;
+typedef complex<double> Point;
 
-  bool res = false;
-  weight[0] = 0.0;
-  weight[1] = 0.0;
-  weight[2] = 0.0;
-
-  for(int round = 0; round < 5000000; round++){
-    bool isok = true;
-    for(int i = 0; i < white.size(); i++){
-      double o = (weight[0] + weight[1] * white[i].first + weight[2] * white[i].second);
-      //white == 0
-      //black == 1
-      if(o > 0){
-	weight[0] -= alpha;
-	weight[1] -= alpha * white[i].first;
-	weight[2] -= alpha * white[i].second;
-	isok = false;
-      }
-    }
-    for(int i = 0;i < black.size(); i++){
-      double o = (weight[0] + weight[1] * black[i].first + weight[2] * black[i].second);
-      //white == 0
-      //black == 1
-      if(o <= 0){
-	weight[0] += alpha;
-	weight[1] += alpha * black[i].first;
-	weight[2] += alpha * black[i].second;
-	isok = false;
-      }
-    }
-    if(isok){
-      res = true;
-      break;
-    }
+namespace std {
+  bool operator < (const Point& a, const Point& b) {
+    return real(a) != real(b) ? real(a) < real(b) : imag(a) < imag(b);
   }
-  return res;
+}
+
+double cross(const Point& a, const Point& b) {
+  return imag(conj(a)*b);
+}
+double dot(const Point& a, const Point& b) {
+  return real(conj(a)*b);
+}
+
+struct Line : public vector<Point> {
+  Line(const Point &a, const Point &b) {
+    push_back(a); push_back(b);
+  }
+};
+
+int ccw(Point a, Point b, Point c) {
+  b -= a; c -= a;
+  if (cross(b, c) > 0)   return +1;       // counter clockwise
+  if (cross(b, c) < 0)   return -1;       // clockwise
+  if (dot(b, c) < 0)     return +2;       // c--a--b on line
+  if (norm(b) < norm(c)) return -2;       // a--b--c on line
+  return 0;
+}
+
+bool is_inner(Point a, Point b, Point c) {
+  b -= a; c -= a;
+  return (dot(b,c) <= 0);
+}
+
+bool intersectSS(const Line &s, const Line &t) {
+  return ccw(s[0],s[1],t[0])*ccw(s[0],s[1],t[1]) <= 0 &&
+    ccw(t[0],t[1],s[0])*ccw(t[0],t[1],s[1]) <= 0;
+}
+
+Point crosspoint(const Line &l, const Line &m) {
+  double A = cross(l[1] - l[0], m[1] - m[0]);
+  double B = cross(l[1] - l[0], l[1] - m[0]);
+  if (abs(A) < EPS && abs(B) < EPS) return m[0]; // same line
+  if (abs(A) < EPS) assert(false); // !!!PRECONDITION NOT SATISFIED!!!
+  return m[0] + B / A * (m[1] - m[0]);
+}
+
+Point projection(const Line &l, const Point &p) {
+  double t = dot(p-l[0], l[0]-l[1]) / norm(l[0]-l[1]);
+  return l[0] + t*(l[0]-l[1]);
+}
+
+bool is_projection_on_line(const Line &l, const Point &p) {
+  Point pj = projection(l,p);
+  return is_inner(pj,l[0],l[1]);
+}
+
+double distanceLP(const Line &l, const Point &p) {
+  return abs(p - projection(l, p));
+}
+
+bool is_equal(const Point &l,const Point &m){
+  return ((abs(real(l) - real(m)) < EPS) && (abs(imag(l) - imag(m) < EPS)));
+}
+
+bool cmp_x(const Point& p, const Point& q){
+  if(p.real() != q.real()) return p.real() < q.real();
+  return p.imag() < q.imag();
+}
+
+vector<Point> compute_convex_hull(vector<Point> ps){
+  sort(ps.begin(), ps.end(), cmp_x);
+  int k = 0;
+  vector<Point> qs(ps.size() * 2);
+  for(int i=0; i < ps.size(); i++){
+    while(k > 1 && cross(qs[k - 1] - qs[k - 2],ps[i] - qs[k - 1]) <= 0) k--;
+    qs[k++] = ps[i];
+  }
+  for(int i = ps.size() - 2,t = k; i >= 0; i--){
+    while(k > t && cross(qs[k - 1] - qs[k - 2],ps[i] - qs[k - 1]) <= 0) k--;
+    qs[k++] = ps[i];
+  }
+  qs.resize(k-1);
+  return qs;
+}
+
+bool is_convex_hull(vector<Point>& ps,const Point& c){
+  for(int i=0;i<ps.size();i++){
+    if(is_equal(ps[i],c)) return true;
+  }
+  return false;
 }
 
 int main(){
@@ -77,19 +132,35 @@ int main(){
   while(~scanf("%d %d",&n,&m)){
     if(n == 0 && m == 0) break;
 
-    vector<P> white;
+    vector<Point> white;
     for(int i = 0; i < n; i++){
       double x,y;
       scanf("%lf %lf",&x,&y);
-      white.push_back(P(x,y));
+      white.push_back(Point(x,y));
     }
-    vector<P> black;
+    vector<Point> white_convex_hull = compute_convex_hull(white);
+
+    vector<Point> black;
     for(int i = 0; i < m; i++){
       double x,y;
       scanf("%lf %lf",&x,&y);
-      black.push_back(P(x,y));
+      black.push_back(Point(x,y));
     }
+    vector<Point> black_convex_hull = compute_convex_hull(black);
 
-    printf("%s\n",McCullochPitts(white,black) ? "YES" : "NO");
+    bool isok = true;
+    for(int i = 0; i < white_convex_hull.size(); i++){
+      for(int j = 0; j < black_convex_hull.size(); j++){
+	for(int k = j + 1; k < black_convex_hull.size(); k++){
+	  if(intersectSS(Line(white_convex_hull[i],white_convex_hull[(i+1) % white_convex_hull.size()]),
+			 Line(black_convex_hull[j],black_convex_hull[k]))){
+	    isok = false;
+	    goto found;
+	  }
+	}
+      }
+    }
+  found:;
+    printf("%s\n",isok ? "YES" : "NO");
   }
 }
