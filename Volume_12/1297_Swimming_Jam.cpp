179d2a6f7881f2cf24c5836e0b70edb776867eee
okaoka <mnfrf593@yahoo.co.jp>
2015-04-02 22:00:05 +0900

    added input portion

---
+++ Volume_12/1297_Swimming_Jam.cpp
@@ -0,0 +1,46 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+#include <iterator>
+#include <complex>
+#include <assert.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <double,double> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {0,1,0,-1};//URDL
+int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int num_of_swimmers;
+  while(~scanf("%d",&num_of_swimmers)){
+    if(num_of_swimmers == 0) break;
+    for(int i = 0; i < num_of_swimmers; i++){
+      int natural_pace;
+      int laps;
+      scanf("%d %d",&natural_pace,&laps);
+    }
+  }
+}
