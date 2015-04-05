4d1bbdad88c39f05359061a114f81be3418716ef
okaoka <mnfrf593@yahoo.co.jp>
2015-04-05 20:45:27 +0900

    added input portion

---
+++ Volume_12/1290_Traveling_Cube.cpp
@@ -0,0 +1,47 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+#include <iterator>
+#include <complex>
+#include <assert.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <double,double> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {0,1,0,-1};//URDL
+int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int H,W;
+  while(~scanf("%d %d",&W,&H)){
+    char stage[31][31];
+    for(int y = 0; y < H; y++){
+      scanf("%s",stage[y]);
+    }
+
+    char order[8];
+    scanf("%s",order);
+  }
+}
