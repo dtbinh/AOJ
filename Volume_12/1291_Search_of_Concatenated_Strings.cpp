7b8367a0846dc06e93cf313ac658119fc768fdc1
okaoka <mnfrf593@yahoo.co.jp>
2015-04-08 22:34:24 +0900

    added input portion

---
+++ Volume_12/1291_Search_of_Concatenated_Strings.cpp
@@ -0,0 +1,53 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+#include <iterator>
+#include <complex>
+#include <assert.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <double,double> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {+0,+1,+0,-1};//URDL
+int ty[] = {-1,+0,+1,+0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int num_of_element_strings;
+  int num_of_lines;
+  while(~scanf("%d %d",&num_of_element_strings,&num_of_lines)){
+    vector<string> elements;
+    for(int i = 0; i < num_of_element_strings; i++){
+      string tmp;
+      cin >> tmp;
+      elements.push_back(tmp);
+    }
+    vector<string> lines;
+    for(int i = 0; i < num_of_lines; i++){
+      string tmp;
+      cin >> tmp;
+      lines.push_back(tmp);
+    }
+  }
+}
