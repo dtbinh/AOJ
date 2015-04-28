fa8169cb8688db1d27cc2db0b64fab1ff465c871
okaoka <mnfrf593@yahoo.co.jp>
2015-04-29 01:17:38 +0900

    outputs of the testcase 1 to 5 is correct

--- Volume_20/2017_Karakuri_Doll.cpp
+++ Volume_20/2017_Karakuri_Doll.cpp
@@ -54,6 +54,7 @@ int main(){
     if(W == 0 && H == 0) break;
     char stage[20][70];
     int sx,sy;
+    int gx,gy;
     for(int y = 0; y < H; y++){
       char line[70];
       scanf("%s",line);
@@ -63,35 +64,80 @@ int main(){
 	  sx = x;
 	  sy = y;
 	}
+	if(stage[y][x] == 'M'){
+	  gx = x;
+	  gy = y;
+	}
       }
     }
 
     priority_queue<State,vector<State>,greater<State> > que;
     que.push(State(sx,sy,0,0));
     bool visited[20][70][4] = {};
+    int flag = 0;
 
     while(!que.empty()){
       State s = que.top();
       que.pop();
+      if(s.x == gx && s.y == gy){
+	flag |= (1<<0);
+	int dx = s.x;
+	int dy = s.y;
+	int dir = (s.dir + 2) % 4;
+	while(true){
+	  if(stage[dy + ty[dir]][dx + tx[dir]] == '#'){
+	    if(s.commands.size() == 0) break;
+	    dir = dir + (s.commands.back() == 0 ? 1 : 3);
+	    dir %= 4;
+	    s.commands.pop_back();
+	  }
+	  else{
+	    dy = dy + ty[dir];
+	    dx = dx + tx[dir];
+	  }
+	  if(dx == sx && dy == sy){
+	    flag |= (1<<1);
+	    break;
+	  }
+	}
+	continue;     
+      }
+
       for(int i = 0; i < 4; i++){
 	int dx = tx[s.dir] + s.x;
 	int dy = ty[s.dir] + s.y;
-	if(dy >= H || dx >= W || dy < 0 || dx > 0) continue;
+	if(dy >= H || dx >= W || dy < 0 || dx < 0) continue;
 	if(stage[dy][dx] == '#'){
-	  if(!visited[dy][dx][(s.dir + 1) % 4]){
+	  if(!visited[s.y][s.x][(s.dir + 1) % 4]){
 	    vector<int> next = s.commands;
 	    next.push_back(1);
-	    visited[dy][dx][(s.dir + 1) % 4] = true;
-	    que.push(State(dx,dy,s.cost + 1,(s.dir + 1) % 4,next));
+	    visited[s.y][s.x][(s.dir + 1) % 4] = true;
+	    que.push(State(s.x,s.y,s.cost + 1,(s.dir + 1) % 4,next));
 	  }
-	  if(!visited[dy][dx][(s.dir + 3) % 4]){
+	  if(!visited[s.y][s.x][(s.dir + 3) % 4]){
 	    vector<int> next = s.commands;
 	    next.push_back(0);
-	    visited[dy][dx][(s.dir + 3) % 4] = true;
-	    que.push(State(dx,dy,s.cost + 1,(s.dir + 3) % 4,next));
+	    visited[s.y][s.x][(s.dir + 3) % 4] = true;
+	    que.push(State(s.x,s.y,s.cost + 1,(s.dir + 3) % 4,next));
 	  }
 	}
+	else if(stage[dy][dx] == '.'
+		|| stage[dy][dx] == 'K'
+		|| stage[dy][dx] == 'M'){
+	  visited[dy][dx][s.dir] = true;
+	  que.push(State(dx,dy,s.cost + 1,s.dir,s.commands));
+	}
       }
+    }
+
+    if(flag == (1<<2) - 1){
+      printf("He can accomplish his mission.\n");
+    }
+    else if(flag == (1<<0)){
+      printf("He cannot return to the kitchen.\n");
+    }
+    else{
+      printf("He cannot bring tea to his master.\n");
     }
   }
 }
