57deb780b3b61c0f008efbbe776e0242881a9de5
okaoka <mnfrf593@yahoo.co.jp>
2015-05-14 00:37:08 +0900

    added input portion

---
+++ Volume_21/2189_Addition_Game.cpp
@@ -0,0 +1,42 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+  
+using namespace std;
+  
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P> PP;
+  
+static const double EPS = 1e-8;
+  
+static const int tx[] = {0,1,0,-1};
+static const int ty[] = {-1,0,1,0};
+
+int main(){
+  int total_test_cases;
+  while(~scanf("%d",&total_test_cases)){
+    for(int test_i = 0; test_i < total_test_cases; test_i++){
+      string stage;
+      cin >> stage;
+    }
+  }
+}
