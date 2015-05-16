217682a81b1fffdfd0972d9a78d0b40b1ee8d27c
okaoka <mnfrf593@yahoo.co.jp>
2015-05-17 02:00:18 +0900

    WA

--- Volume_21/2190_Angel_Stairs.cpp
+++ Volume_21/2190_Angel_Stairs.cpp
@@ -31,8 +31,54 @@ static const double EPS = 1e-8;
 static const int tx[] = {0,1,0,-1};
 static const int ty[] = {-1,0,1,0};
 
+class State {
+public:
+  int stair_pos;
+  int note_pos;
+  int cost;
+  State(int stair_pos,int note_pos,int cost) :
+    stair_pos(stair_pos),note_pos(note_pos), cost(cost) {}
+  bool operator<(const State& s) const {
+    return cost < s.cost;
+  }
+  bool operator>(const State& s) const {
+    return cost > s.cost;
+  }
+};
+
+map<string,int> note2idx;
+string idx2note[] = {
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B"
+};
+
+map<int,bool> dp[50001];
+
 int main(){
   int total_test_cases;
+  note2idx["C"] = 0;
+  note2idx["C#"] = 1;
+  note2idx["D"] = 2;
+  note2idx["D#"] = 3;
+  note2idx["E"] = 4;
+  note2idx["F"] = 5;
+  note2idx["F#"] = 6;
+  note2idx["G"] = 7;
+  note2idx["G#"] = 8;
+  note2idx["A"] = 9;
+  note2idx["A#"] = 10;
+  note2idx["B"] = 11;
+
   while(~scanf("%d",&total_test_cases)){
     for(int test_i = 0; test_i < total_test_cases; test_i++){
       int num_of_steps;
@@ -50,6 +96,37 @@ int main(){
 	cin >> note;
 	song.push_back(note);
       }
+
+      priority_queue<State> que;
+      que.push(State(-1,-1,0));
+
+      bool isok = false;
+      while(!que.empty()){
+	State s = que.top();
+	que.pop();
+
+	if(s.note_pos == song_length - 1
+	   && s.stair_pos >= num_of_steps - 2){
+	  isok = true;
+	  break;
+	}
+	for(int dist = -1; dist <= 2; dist++){
+	  if(dist == 0) continue;
+
+	  int next = s.stair_pos + dist;
+	  if(next < 0) continue;
+	  if(next >= num_of_steps) continue;
+
+	  int offset = 0;
+	  if(dist == -1) offset = 12 - 1;
+	  if(dist == 2) offset = 1;
+	  if(song[s.note_pos + 1] != idx2note[(note2idx[stair[next]] + offset) % 12]) continue;
+	  if(dp[s.note_pos + 1].find(next) != dp[s.note_pos + 1].end()) continue;
+	  dp[s.note_pos + 1][next] = true;
+	  que.push(State(next,s.note_pos + 1,s.cost + 1));
+	}
+      }
+      printf("%s\n",isok ? "Yes" : "No");
     }
   }
 }
