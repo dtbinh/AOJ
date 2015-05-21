799caac84c64b2f0b76c31da75c9da83f8ff9a58
okaoka <mnfrf593@yahoo.co.jp>
2015-05-22 00:38:14 +0900

    updated

--- Volume_21/2107_Can_I_go_there.cpp
+++ Volume_21/2107_Can_I_go_there.cpp
@@ -31,6 +31,31 @@ static const double EPS = 1e-8;
 static const int tx[] = {0,1,0,-1};
 static const int ty[] = {-1,0,1,0};
 
+int matrix[501][501];
+
+struct State {
+  int from;
+  int to;
+  State(int f,int t)
+    : from(f),to(t) {}
+};
+
+void matrix_pow(int length){
+  int tmp[501][501] = {};
+  for(int i = 0; i < length; i++){
+    for(int j = 0; j < length; j++){
+      int sum = 0;
+      for(int k = 0; k < length; k++){
+	sum += matrix[i][k] * matrix[k][j];
+      }
+      tmp[i][j] = sum;
+    }
+  }
+  memcpy(matrix,tmp,sizeof(int)*501*501);
+}
+
+
+
 int main(){
   int total_stations;
   int total_sections;
@@ -40,9 +65,46 @@ int main(){
 	       &total_sections,
 	       &total_steps)){
     if(total_stations == 0 && total_sections == 0 && total_steps == 0) break;
+    memset(matrix,0,sizeof(matrix));
+    vector<int> edge[51];
     for(int section_i = 0; section_i < total_sections; section_i++){
       int src,dst;
       scanf("%d %d",&src,&dst);
+      src--; dst--;
+      edge[src].push_back(dst);
+      edge[dst].push_back(src);
     }
+
+    vector<State> states;
+    states.push_back(State(-1,0));
+    for(int from = 0; from < total_stations; from++){
+      for(int i = 0; i < edge[from].size(); i++){
+	int to = edge[from][i];
+	states.push_back(State(from,to));
+      }
+    }
+
+    
+    for(int y = 0; y < states.size(); y++){
+      for(int x = 0; x < states.size(); x++){
+	if(states[y].to == states[x].from
+	   && states[y].from != states[x].to){
+	  matrix[y][x] = 1;
+	}
+      }
+    }
+
+    for(int i = 0; i < total_steps - 1; i++){
+      matrix_pow(states.size());
+    }
+
+    bool isok = false;
+    for(int i = 0; i < states.size(); i++){
+      if(states[i].to == total_stations - 1 && matrix[0][i] == 1){
+	isok = true;
+	break;
+      }
+    }
+    printf("%s\n",isok ? "yes" : "no");
   }
 }
