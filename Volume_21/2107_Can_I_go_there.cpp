ea3f5ce5b15e4ed07fa546a9a2005b157aa7c9b6
okaoka <mnfrf593@yahoo.co.jp>
2015-05-23 02:19:00 +0900

    WA

--- Volume_21/2107_Can_I_go_there.cpp
+++ Volume_21/2107_Can_I_go_there.cpp
@@ -31,8 +31,6 @@ static const double EPS = 1e-8;
 static const int tx[] = {0,1,0,-1};
 static const int ty[] = {-1,0,1,0};
 
-int matrix[501][501];
-
 struct State {
   int from;
   int to;
@@ -40,21 +38,43 @@ struct State {
     : from(f),to(t) {}
 };
 
-void matrix_pow(int length){
-  int tmp[501][501] = {};
+void matrix_pow(const int A[101][101],const int B[101][101],
+		int C[101][101],int length){
+  int tmp[101][101] = {};
   for(int i = 0; i < length; i++){
     for(int j = 0; j < length; j++){
       int sum = 0;
       for(int k = 0; k < length; k++){
-	sum += matrix[i][k] * matrix[k][j];
+	sum += A[i][k] * B[k][j];
       }
       tmp[i][j] = sum;
     }
   }
-  memcpy(matrix,tmp,sizeof(int)*501*501);
+  memcpy(C,tmp,sizeof(int)*101*101);
 }
 
+void fast_pow(int n,int length,int A[101][101],int C[101][101]) {
+  for(int i = 0; i < length; i++){
+    C[i][i] = 1;
+  }
+  while(n > 0) {
+    if (n & 1){
+      matrix_pow(C,A,C,length);
+    }
+    matrix_pow(A,A,A,length);
+    n >>= 1;
+  }
+}
 
+void disp(int matrix[101][101]){
+  for(int y = 0; y < 10; y++){
+    for(int x = 0; x < 10; x++){
+      printf("%d ",matrix[y][x]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
 
 int main(){
   int total_stations;
@@ -65,8 +85,7 @@ int main(){
 	       &total_sections,
 	       &total_steps)){
     if(total_stations == 0 && total_sections == 0 && total_steps == 0) break;
-    memset(matrix,0,sizeof(matrix));
-    vector<int> edge[51];
+    vector<int> edge[101];
     for(int section_i = 0; section_i < total_sections; section_i++){
       int src,dst;
       scanf("%d %d",&src,&dst);
@@ -84,27 +103,27 @@ int main(){
       }
     }
 
-    
+    int init_matrix[101][101] = {};
     for(int y = 0; y < states.size(); y++){
       for(int x = 0; x < states.size(); x++){
 	if(states[y].to == states[x].from
 	   && states[y].from != states[x].to){
-	  matrix[y][x] = 1;
+	  init_matrix[y][x] = 1;
 	}
       }
     }
 
-    for(int i = 0; i < total_steps - 1; i++){
-      matrix_pow(states.size());
-    }
+    int result_matrix[101][101] = {};
+    fast_pow(total_steps,states.size(),init_matrix,result_matrix);
 
     bool isok = false;
     for(int i = 0; i < states.size(); i++){
-      if(states[i].to == total_stations - 1 && matrix[0][i] == 1){
+      if(states[i].to == total_stations - 1 && result_matrix[0][i] > 0){
 	isok = true;
 	break;
       }
     }
+    // disp(result_matrix);
     printf("%s\n",isok ? "yes" : "no");
   }
 }
