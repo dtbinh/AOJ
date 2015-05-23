6441c613edd2e6d8a9c7c30ca815f21ed06d61a4
okaoka <mnfrf593@yahoo.co.jp>
2015-05-23 21:18:11 +0900

    added input portion

---
+++ Volume_21/2191_A_Book_Shop_With_a_Frequent_Greetings.cpp
@@ -0,0 +1,44 @@
+#define _USE_MATH_DEFINES
+#define INF 0x3f3f3f3f
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <map>
+#include <string>
+#include <cstring>
+#include <set>
+#include <deque>
+#include <bitset>
+#include <list>
+#include <cctype>
+#include <utility>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair <int,int> P;
+typedef pair <int,P > PP;
+ 
+int tx[] = {0,1,0,-1};
+int ty[] = {-1,0,1,0};
+ 
+static const double EPS = 1e-8;
+
+int main(){
+  int total_staff;
+  int speech_duration,prohibited_duration;
+  while(~scanf("%d %d %d",&total_staff,
+	       &speech_duration,&prohibited_duration)){
+    int customer_x,customer_y;
+    scanf("%d %d",&customer_x,&customer_y);
+    for(int staff_i = 0; staff_i < total_staff; staff_i++){
+      scanf("%d %d",&x,&y);
+    }
+  }
+}
