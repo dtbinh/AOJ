d951043e096681401c95900cecd88421e15a2aa5
okaoka <mnfrf593@yahoo.co.jp>
2015-05-25 00:30:30 +0900

    updated

--- Volume_21/2191_A_Book_Shop_With_a_Frequent_Greetings.cpp
+++ Volume_21/2191_A_Book_Shop_With_a_Frequent_Greetings.cpp
@@ -18,6 +18,7 @@
 #include <list>
 #include <cctype>
 #include <utility>
+#include <complex>
  
 using namespace std;
  
@@ -31,14 +32,65 @@ int ty[] = {-1,0,1,0};
 static const double EPS = 1e-8;
 
 int main(){
-  int total_staff;
-  int speech_duration,prohibited_duration;
-  while(~scanf("%d %d %d",&total_staff,
-	       &speech_duration,&prohibited_duration)){
-    int customer_x,customer_y;
-    scanf("%d %d",&customer_x,&customer_y);
-    for(int staff_i = 0; staff_i < total_staff; staff_i++){
-      scanf("%d %d",&x,&y);
+  int total_test_cases;
+  while(~scanf("%d",&total_test_cases)){
+    for(int test_i = 0; test_i < total_test_cases; test_i++){
+      int total_staff;
+      int speech_duration,prohibited_duration;
+
+      scanf("%d %d %d",&total_staff,
+	    &speech_duration,&prohibited_duration);
+      int customer_x,customer_y;
+      scanf("%d %d",&customer_x,&customer_y);
+      vector<complex<int> > staff;
+      for(int staff_i = 0; staff_i < total_staff; staff_i++){
+	int x,y;
+	scanf("%d %d",&x,&y);
+	staff.push_back(complex<int>(x,y));
+      }
+      
+      int latest_speech_finished_time[1001];
+      memset(latest_speech_finished_time,-1,sizeof(latest_speech_finished_time));
+      queue<int> que;
+      for(int staff_i = 0; staff_i < total_staff; staff_i++){
+	if(abs(staff[staff_i] - complex<int>(customer_x,customer_y)) > 10) continue;
+	que.push(staff_i);
+	latest_speech_finished_time[staff_i] = speech_duration;
+      }
+      
+      vector<int> edges[1001];
+      for(int staff_i = 0; staff_i < total_staff; staff_i++){
+	for(int staff_j = 0; staff_j < total_staff; staff_j++){
+	  if(staff_i == staff_j) continue;
+	  if(abs(staff[staff_i] - staff[staff_j]) > 50) continue;
+	  edges[staff_i].push_back(staff_j);
+	}
+      }
+      
+      for(int round = 0; round < 100000; round++){
+	queue<int> next;
+	while(!que.empty()){
+	  int staff_i = que.front();
+	  que.pop();
+	  for(int i = 0; i < edges[staff_i].size(); i++){
+	    int staff_j = edges[staff_i][i];
+	    if(latest_speech_finished_time[staff_j] == -1 
+	       || (latest_speech_finished_time[staff_i] + prohibited_duration
+		   <= latest_speech_finished_time[staff_j])){
+	      next.push(staff_j);
+	      latest_speech_finished_time[staff_j]
+		= latest_speech_finished_time[staff_i] + 2 * speech_duration;
+	    }
+	  }
+	}
+	que = next;
+      }
+      
+      int res = 0;
+      for(int i = 0; i < total_staff; i++){
+	res = max(res,latest_speech_finished_time[i]);
+      }
+      printf("%d\n",res);
     }
   }
 }
